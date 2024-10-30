32c44fb02f37cb8de199c7ad1c8c5970e3dca058
liym27 <33742067+liym27@users.noreply.github.com>
2024-10-30 17:52:37 +0800

    fix

--- paddle/fluid/framework/new_executor/interpreter/static_build.cc
+++ paddle/fluid/framework/new_executor/interpreter/static_build.cc
@@ -45,6 +45,7 @@ std::set<std::string> OpsCanSkipedFakeAllocInStaticBuild = {
     "c_gen_bkcl_id",
     "c_gen_nccl_id",
     "sync_calc_stream",
+    "c_sync_calc_stream",
     "c_sync_comm_stream",
     "c_wait_comm",
     "c_wait_compute",
