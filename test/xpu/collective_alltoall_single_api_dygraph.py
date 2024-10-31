674762a29f8c831ca116442c35f99121e360a24d
ShenLiang <1422485404@qq.com>
2024-10-31 16:39:29 +0800

    fix a2a api

--- test/xpu/collective_alltoall_single_api_dygraph.py
+++ test/xpu/collective_alltoall_single_api_dygraph.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import legacy_test.test_collective_api_base as test_base
+import test_collective_api_base as test_base
 
 import paddle
 import paddle.distributed as dist
