a289b64de473074d809edf983749e057d499bd5a
lizexu <2694294196@qq.com>
2024-11-04 03:15:32 +0000

    fix

--- paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
+++ paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
@@ -80,6 +80,7 @@ DEFINE_GENERAL_PATTERN(Hardswish, paddle::dialect::HardswishOp)
 DEFINE_GENERAL_PATTERN(Assign, paddle::dialect::AssignOp)
 DEFINE_GENERAL_PATTERN(AssignValue_, paddle::dialect::AssignValue_Op)
 DEFINE_GENERAL_PATTERN(AssignOut, paddle::dialect::AssignOut_Op)
+DEFINE_GENERAL_PATTERN(Roll,paddle::dialect::RollOp)
 
 #undef DEFINE_GENERAL_PATTERN
 
@@ -1625,6 +1626,7 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ADD_PATTERN(AssignOut)
     ADD_PATTERN(Assign)
     ADD_PATTERN(AssignValue_)
+    ADD_PATTERN(Roll)
 #if IS_TRT_VERSION_GE(8600)
     ADD_PATTERN(Layer_norm)
 #endif
