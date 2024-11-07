95b90492601dfe00182ba5b6ba3ebbaefd6c4923
phlrain <phliuhongyu@126.com>
2024-11-07 10:31:37 +0800

    add type check

--- paddle/cinn/hlir/framework/pir/utils.cc
+++ paddle/cinn/hlir/framework/pir/utils.cc
@@ -431,8 +431,8 @@ bool IsComplex(const ::pir::Operation& op) {
                        .type()
                        .dyn_cast<paddle::dialect::DenseTensorType>()
                        .dtype();
-      if (dtype.isa<::pir::Complex64Type>() ||
-          dtype.isa<::pir::Complex128Type>()) {
+      if (type && (dtype.isa<::pir::Complex64Type>() ||
+                   dtype.isa<::pir::Complex128Type>())) {
         return true;
       }
     }
@@ -444,8 +444,8 @@ bool IsComplex(const ::pir::Operation& op) {
                        .type()
                        .dyn_cast<paddle::dialect::DenseTensorType>()
                        .dtype();
-      if (dtype.isa<::pir::Complex64Type>() ||
-          dtype.isa<::pir::Complex128Type>()) {
+      if (type && (dtype.isa<::pir::Complex64Type>() ||
+                   dtype.isa<::pir::Complex128Type>())) {
         return true;
       }
     }
