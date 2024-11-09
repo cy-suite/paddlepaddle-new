f884a7b1c6c446658c5a157097314f7e60a7f3ba
ooooo <3164076421@qq.com>
2024-11-09 13:52:24 +0800

    fix

--- paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
+++ paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
@@ -1755,6 +1755,7 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ps.Add(std::make_unique<MulticlassNms3OpPattern>(context));
     ps.Add(std::make_unique<ArgmaxOpPattern>(context));
     ps.Add(std::make_unique<ArgminOpPattern>(context));
+    ps.Add(std::make_unique<ArgsortOpPattern>(context));
     ps.Add(std::make_unique<MaxOpPattern>(context));
     ps.Add(std::make_unique<MinOpPattern>(context));
     ps.Add(std::make_unique<AllOpPattern>(context));
