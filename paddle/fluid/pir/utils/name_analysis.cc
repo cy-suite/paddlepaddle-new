6521c9902a89e634bc1d9606cc628b71a396efe8
risemeup1 <515586620@qq.com>
2024-11-13 10:38:49 +0000

    refine optimization ouput

--- paddle/fluid/pir/utils/name_analysis.cc
+++ paddle/fluid/pir/utils/name_analysis.cc
@@ -14,7 +14,8 @@
 
 #include "paddle/fluid/pir/utils/name_analysis.h"
 #include "paddle/fluid/pir/dialect/operator/ir/pd_op.h"
-
+#include "paddle/fluid/pir/dialect/operator/trait/inplace.h"
+#include "paddle/fluid/pir/dialect/operator/utils/op_yaml_info_parser.h"
 namespace pir {
 namespace utils {
 namespace name_analysis {
@@ -206,6 +207,33 @@ std::map<std::string, std::string> RenameValue(Value value,
   return rename_mapping;
 }
 
+std::optional<std::string> GetCorrespondingInplaceName(pir::Operation *op) {
+  pir::IrContext *ctx = pir::IrContext::Instance();
+  auto op_name = op->name();
+  pir::OpInfo op_info = ctx->GetRegisteredOpInfo(op_name);
+  paddle::dialect::OpYamlInfoParser yaml_parser(
+      op_info.GetInterfaceImpl<paddle::dialect::OpYamlInfoInterface>()
+          ->get_op_info_(op_name),
+      paddle::dialect::IsLegacyOp(op_name));
+  for (size_t i = 0; i < op->num_results(); ++i) {
+    pir::Value value = op->result(i);
+    std::string value_name = yaml_parser.OutputNames()[i];
+    if (yaml_parser.HasInplace(value_name)) {
+      const std::string &inplace_name = yaml_parser.InplaceName(value_name);
+      pir::Value inplace_value =
+          op->operand_source(yaml_parser.InputName2Id().at(inplace_name));
+      pir::Operation *owner = inplace_value.defining_op();
+      if (owner->isa<pir::ParameterOp>()) {
+        pir::ParameterOp parameter_op = owner->dyn_cast<pir::ParameterOp>();
+        // auto value = parameter_op->result(0);
+        std::string name = parameter_op.param_name();
+        return name;
+      }
+    }
+  }
+  return std::nullopt;
+}
+
 std::optional<std::string> GetValueInputName(pir::Value value) {
   std::optional<std::string> name;
   if (auto block_arg = value.dyn_cast<pir::BlockArgument>()) {
@@ -220,6 +248,9 @@ std::optional<std::string> GetValueInputName(pir::Value value) {
     name = data_op.attribute<pir::StrAttribute>("name").AsString();
   } else if (auto constant_op = value.defining_op<::pir::ConstantTensorOp>()) {
     name = constant_op.tensor_name();
+  } else if (value.defining_op()->HasTrait<paddle::dialect::InplaceTrait>()) {
+    auto defining_op = value.defining_op();
+    name = GetCorrespondingInplaceName(defining_op);
   }
   return name;
 }
@@ -268,7 +299,11 @@ std::optional<std::string> TryGetValueFirstName(pir::Value value) {
 
 std::string GetValueFirstName(pir::Value value) {
   auto name = TryGetValueFirstName(value);
-
+  if (name.has_value()) {
+    std::cout << name.value() << std::endl;
+  } else {
+    std::cout << "No value" << std::endl;
+  }
   PADDLE_ENFORCE(name.has_value(),
                  common::errors::InvalidArgument(
                      "Currently, we can only get name of Value from "
