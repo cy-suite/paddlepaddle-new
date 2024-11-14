afedb8d6baf0bbfd7635514c237dbed49fe4d9b6
feixi21 <1802550529@qq.com>
2024-11-14 16:52:22 +0800

    modify according to comment
    modify according to comment

--- paddle/cinn/common/target.cc
+++ paddle/cinn/common/target.cc
@@ -24,6 +24,7 @@
 
 #include "paddle/cinn/backends/cuda_util.h"
 #include "paddle/cinn/common/arch_util.h"
+#include "paddle/cinn/common/macros.h"
 #include "paddle/cinn/common/target.h"
 #include "paddle/cinn/runtime/backend_api.h"
 #include "paddle/cinn/runtime/cinn_runtime.h"
