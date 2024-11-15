c8c570cac9c9b7e490b53dae7fd71b2c99d7a288
MrXnneHang <XnneHang@gmail.com>
2024-11-15 16:08:31 +0800

    refact: move `paddle.tensor.logic.positive` to `paddle.tensor.math.positive`

--- python/paddle/tensor/__init__.py
+++ python/paddle/tensor/__init__.py
@@ -141,7 +141,6 @@ from .logic import (  # noqa: F401
     logical_xor_,
     not_equal,
     not_equal_,
-    positive,
 )
 from .manipulation import (  # noqa: F401
     as_complex,
@@ -366,6 +365,7 @@ from .math import (  # noqa: F401
     outer,
     polygamma,
     polygamma_,
+    positive,
     pow,
     pow_,
     prod,
