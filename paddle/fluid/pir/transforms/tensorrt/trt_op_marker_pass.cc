7b7196ed2d587cbf9757433502d39048cec3cdba
ooooo <3164076421@qq.com>
2024-11-16 12:37:39 +0800

    refine code

--- paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
+++ paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
@@ -86,7 +86,8 @@ DEFINE_GENERAL_PATTERN(Swish, paddle::dialect::SwishOp)
 DEFINE_GENERAL_PATTERN(Log, paddle::dialect::LogOp)
 DEFINE_GENERAL_PATTERN(Floor, paddle::dialect::FloorOp)
 DEFINE_GENERAL_PATTERN(Roll, paddle::dialect::RollOp)
-DEFINE_GENERAL_PATTERN(Logsigmoid, paddle::dialect::LogsigmoidOp)
+DEFINE_GENERAL_PATTERN(Softplus, paddle::dialect::SoftplusOp)
+DEFINE_GENERAL_PATTERN(LeakyRelu, paddle::dialect::LeakyReluOp)
 
 #undef DEFINE_GENERAL_PATTERN
 
@@ -259,8 +260,7 @@ class ActOpPattern : public pir::OpRewritePattern<OpType> {
 };
 
 using TanhOpPattern = ActOpPattern<paddle::dialect::TanhOp>;
-using SoftplusOpPatten = ActOpPattern<paddle::dialect::SoftplusOp>;
-using LeakyReluOpPattern = ActOpPattern<paddle::dialect::LeakyReluOp>;
+using LogsigmoidOpPattern = ActOpPattern<paddle::dialect::LogsigmoidOp>;
 
 class Pool2dOpPattern
     : public pir::OpRewritePattern<paddle::dialect::Pool2dOp> {
@@ -1880,7 +1880,8 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ADD_PATTERN(Log)
     ADD_PATTERN(Floor)
     ADD_PATTERN(Roll)
-    ADD_PATTERN(Logsigmoid)
+    ADD_PATTERN(Softplus)
+    ADD_PATTERN(LeakyRelu)
 #if IS_TRT_VERSION_GE(8600)
     ADD_PATTERN(Layer_norm)
 #endif
@@ -1938,10 +1939,9 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ps.Add(std::make_unique<FullWithTensorPattern>(context));
     ps.Add(std::make_unique<StridedSliceOpPattern>(context));
     ps.Add(std::make_unique<TopkOpPattern>(context));
-    ps.Add(std::make_unique<SoftplusOpPatten>(context));
     ps.Add(std::make_unique<EqualOpPattern>(context));
     ps.Add(std::make_unique<NotEqualOpPattern>(context));
-    ps.Add(std::make_unique<LeakyReluOpPattern>(context));
+    ps.Add(std::make_unique<LogsigmoidOpPattern>(context));
     return ps;
   }
 };
