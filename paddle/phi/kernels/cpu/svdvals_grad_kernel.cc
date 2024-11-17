46d6fffe8fafd256b7120a24f92427d461e1d0e6
Kyotak <2023352046@email.szu.edu.cn>
2024-11-17 23:13:29 +0800

    fix bug

--- paddle/phi/kernels/cpu/svdvals_grad_kernel.cc
+++ paddle/phi/kernels/cpu/svdvals_grad_kernel.cc
@@ -1,5 +1,6 @@
-#include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/svdvals_grad_kernel.h"
+#include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/impl/svdvals_grad_kernel_impl.h"
 
 PD_REGISTER_KERNEL(
     svdvals_grad, CPU, ALL_LAYOUT, phi::SvdvalsGradKernel, float, double) {}
