963e03a9867563ea1ff6bb56380548874f62b5a6
Kyotak <2023352046@email.szu.edu.cn>
2024-11-19 16:43:27 +0800

    fix bug

--- paddle/phi/kernels/impl/svdvals_grad_kernel_impl.h
+++ paddle/phi/kernels/impl/svdvals_grad_kernel_impl.h
@@ -19,7 +19,7 @@ void SvdvalsGradKernel(const Context& dev_ctx,
                        const DenseTensor& s,
                        const paddle::optional<DenseTensor>& s_grad,
                        DenseTensor* x_grad) {
-    if (!s_grad.get_ptr() || s_grad.get_ptr()->numel() == 0) {
+  if (!s_grad.get_ptr() || s_grad.get_ptr()->numel() == 0) {
     funcs::SetConstant<Context, T>()(dev_ctx, x_grad, T(0.0));
     x_grad->Resize(x.dims());
     return;
@@ -29,12 +29,12 @@ void SvdvalsGradKernel(const Context& dev_ctx,
   DenseTensor dX_term = Diag<T, Context>(dev_ctx, dS, 0, 0);
 
   DenseTensor U, VH, S_recomputed;
-  phi::SvdKernel(dev_ctx,
-                 x,
-                 false,
-                 &U,
-                 &S_recomputed,
-                 &VH);  // Crucial: recomputing SVD
+  phi::SvdKernel<T>(dev_ctx,
+                    x,
+                    false,
+                    &U,
+                    &S_recomputed,
+                    &VH);  // Crucial: recomputing SVD
 
   *x_grad =
       Matmul<T, Context>(dev_ctx, Matmul<T, Context>(dev_ctx, U, dX_term), VH);
