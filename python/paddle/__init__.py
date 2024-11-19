2fafae30f2215d02f35518e37cdc99439c404ea2
Sylence8 <1656331403@qq.com>
2024-11-19 13:48:15 +0800

    Update __init__.py

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import typing
 import math
+import typing
 
 __is_metainfo_generated = False
 try:
