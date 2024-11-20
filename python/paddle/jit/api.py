02e1ca67755b7621557df85491f189b77799cfa7
Nyakku Shigure <sigure.qaq@gmail.com>
2024-11-20 23:07:56 +0800

    Update python/paddle/jit/api.py
    Co-authored-by: gouzil <66515297+gouzil@users.noreply.github.com>

--- python/paddle/jit/api.py
+++ python/paddle/jit/api.py
@@ -294,7 +294,7 @@ def to_static(
 
         if sys.version_info >= (3, 14) and not full_graph:
             warnings.warn(
-                "full_graph=False is not supported in Python 3.13+. Set full_graph=True automatically"
+                "full_graph=False is not supported in Python 3.14+. Set full_graph=True automatically"
             )
             full_graph = True
 
