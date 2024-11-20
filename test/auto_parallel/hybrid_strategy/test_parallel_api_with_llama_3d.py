eafa5a9dccb76036b1edf2e8d583a8fa1e769ab2
ooooo <3164076421@qq.com>
2024-11-20 22:16:45 +0800

    Merge branch 'develop' of https://github.com/PaddlePaddle/Paddle into tmp1

--- test/auto_parallel/hybrid_strategy/test_parallel_api_with_llama_3d.py
+++ test/auto_parallel/hybrid_strategy/test_parallel_api_with_llama_3d.py
@@ -12,24 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import tempfile
 import unittest
 
 import collective.test_communication_api_base as test_base
 
-os.environ['FLAGS_enable_pir_api'] = '0'
 
-
-class TestTensorParallelAPI(test_base.CommunicationTestDistBase):
+class TestDPMPPPAPI(test_base.CommunicationTestDistBase):
     def setUp(self):
-        super().setUp(num_of_devices=2, timeout=120, nnode=1)
+        super().setUp(num_of_devices=8, timeout=120, nnode=1)
         self._default_envs = {
             "dtype": "float32",
             "seed": "2023",
-            "dp": "1",
+            "dp": "2",
             "mp": "2",
-            "pp": "1",
+            "pp": "2",
             "acc_step": "2",
         }
         self._changeable_envs = {
@@ -38,26 +35,17 @@ class TestTensorParallelAPI(test_base.CommunicationTestDistBase):
             "amp_level": ["O2"],
             "amp_dtype": ["bfloat16"],
             "amp_master_grad": ["true"],
-            "use_lazy_init": ["true", "false"],
-            "sequence_parallel": ["true", "false"],
-            "prepare_input_output": ["true", "false"],
+            "use_lazy_init": ["true"],
+            "sequence_parallel": ["true"],
+            "prepare_input_output": ["false"],
+            "sharding_stage": ["0", "1"],
         }
 
-    def test_simple_net_mp2(self):
+    def test_simple_net_dp2_mp2_pp2(self):
         envs_list = test_base.gen_product_envs_list(
             self._default_envs, self._changeable_envs
         )
-        has_checked_lazy_init = False
-        has_checked_pure_mp = False
         for envs in envs_list:
-            if envs['use_lazy_init'] == 'true':
-                if has_checked_lazy_init:
-                    continue
-                has_checked_lazy_init = True
-            if envs['sequence_parallel'] != 'true':
-                if has_checked_pure_mp:
-                    continue
-                has_checked_pure_mp = True
             ckpt_path = tempfile.TemporaryDirectory()
             envs["ckpt_path"] = ckpt_path.name
             self.run_test_case(
