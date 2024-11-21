af8d632def88639d1d7061a69bd67a01a2d9d057
ooooo <3164076421@qq.com>
2024-11-21 12:19:34 +0800

    Fix

--- test/tensorrt/test_converter_input.py
+++ test/tensorrt/test_converter_input.py
@@ -21,7 +21,7 @@ import paddle
 
 
 def embedding_warpper_func(x):
-    layer = paddle.nn.Embedding(4, 3)
+    layer = paddle.nn.Embedding(64, 4)
     return layer(x)
 
 
@@ -49,7 +49,12 @@ class TestEmbeddingCase2TRTPattern(TensorRTBaseTest):
     def setUp(self):
         self.python_api = embedding_warpper_func
         self.api_args = {
-            "x": np.random.randn(2, 3).astype("int64"),
+            "x": np.array([[3, 16, 24], [6, 4, 47]]).astype("int64"),
+        }
+        self.dynamic_shape_data = {
+            "x": lambda shape: np.random.randint(1, 64, size=shape).astype(
+                np.int64
+            ),
         }
         self.program_config = {"feed_list": ["x"]}
         self.min_shape = {"x": [1, 3]}
