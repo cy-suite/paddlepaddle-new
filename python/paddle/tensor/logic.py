f689980116b41939c9b483e327957555dbd87f0d
SCUcookie <2450603126@qq.com>
2024-11-21 09:55:06 +0000

    precommit

--- python/paddle/tensor/logic.py
+++ python/paddle/tensor/logic.py
@@ -1233,7 +1233,9 @@ def bitwise_and(
 
 def __rand__(x: Tensor, y: int | bool):
     if not isinstance(y, (int, bool)):
-        raise TypeError(f"unsupported operand type(s) for |: '{type(y).__name__}' and 'Tensor'")
+        raise TypeError(
+            f"unsupported operand type(s) for |: '{type(y).__name__}' and 'Tensor'"
+        )
     else :
         y_tensor = paddle.to_tensor(y, dtype=x.dtype)
         return bitwise_and(y_tensor, x, None, None)
