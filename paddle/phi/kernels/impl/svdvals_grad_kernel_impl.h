e439a2be79a4da159968f4e2773efdb2f057f16b
Kyotak <2023352046@email.szu.edu.cn>
2024-11-22 19:46:02 +0800

    fix

--- paddle/phi/kernels/impl/svdvals_grad_kernel_impl.h
+++ paddle/phi/kernels/impl/svdvals_grad_kernel_impl.h
@@ -47,7 +47,7 @@ void SvdvalsGradKernel(const Context& dev_ctx,
   DenseTensor U, VH, S_recomputed;
   DDim u_dims = {rows, k};
   DDim s_dims = {k, k};
-  DDim vh_dims = {k, col};
+  DDim vh_dims = {k, cols};
   U.Resize(u_dims);
   VH.Resize(vh_dims);
   S_recomputed.Resize(s_dims);
