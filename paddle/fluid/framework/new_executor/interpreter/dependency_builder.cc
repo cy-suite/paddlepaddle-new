61cd4691a822b3c93385e9676bb771575a5018b1
fangfangssj <1135470306@qq.com>
2024-11-22 11:11:51 +0000

    Typos B3-B7

--- paddle/fluid/framework/new_executor/interpreter/dependency_builder.cc
+++ paddle/fluid/framework/new_executor/interpreter/dependency_builder.cc
@@ -784,8 +784,8 @@ void DependencyBuilderSimplify::GetAllbehind() {
   };
   for (size_t i = start_index_; i < op_num_; i++) {
     auto& behinds = ops_behind_[i];
-    auto& befores = ops_before_[i];
-    for (auto before_op : befores) {
+    auto& before = ops_before_[i];
+    for (auto before_op : before) {
       for (auto behind_op : behinds) {
         update_op_happen_before(before_op, behind_op);
       }
