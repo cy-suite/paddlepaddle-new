98fd67d2dc271bb4e4b2fc1b603213726214e631
ooooo <3164076421@qq.com>
2024-11-22 14:05:27 +0800

    apply review

--- python/paddle/jit/dy2static/program_translator.py
+++ python/paddle/jit/dy2static/program_translator.py
@@ -120,6 +120,13 @@ def check_view_api_used_by_inplace(program: paddle.pir.Program) -> None:
         a = transpose(b)
         b.add_(c)
     """
+    # TODO(ooooo): Deal with these inplace ops
+    skipped_inplace_ops = [
+        "pd_op.set_value_",
+        "pd_op.set_value_with_tensor_",
+        # It willn't change tensor imdeiately,but it's ouput is dangerous.
+        "pd_op.share_data_",
+    ]
 
     def val_is_used_by_stride_op(op, val):
         return op.name() in framework.stride_ops and op.operand_source(
@@ -139,13 +146,7 @@ def check_view_api_used_by_inplace(program: paddle.pir.Program) -> None:
             if val_is_used_by_stride_op(op, value):
                 uesd_by_stride_ops.append(op)
             if is_used_by_inplace_op(op, value, inplace_info):
-                # TODO(ooooo): Deal with these inplace ops
-                if (
-                    op.name() == "pd_op.set_value_"
-                    or op.name() == "pd_op.set_value_with_tensor_"
-                    # It willn't change tensor imdeiately,but it's ouput is dangerous.
-                    or op.name() == "pd_op.share_data_"
-                ):
+                if op.name() in skipped_inplace_ops:
                     continue
                 if value.get_defining_op().name() in framework.stride_ops:
                     show_op_callstack(op)
