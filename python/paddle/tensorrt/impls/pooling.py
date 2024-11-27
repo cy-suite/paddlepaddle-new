5b37110a28bd793b031b28debcc288afdcf28053
lizexu <2694294196@qq.com>
2024-11-27 07:16:33 +0000

    fix

--- python/paddle/tensorrt/impls/pooling.py
+++ python/paddle/tensorrt/impls/pooling.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
 
 import tensorrt as trt
 
@@ -21,15 +22,18 @@ from paddle.tensorrt.register import converter_registry
 @converter_registry.register("pd_op.pool2d", trt_version="8.x")
 def pool2d_converter(network, paddle_op, inputs):
     input_tensor = inputs[0]
-    pooling_type = paddle_op.attrs().get("pooling_type", "max")
-    padding = paddle_op.attrs().get("paddings", [0, 0])
-    stride = paddle_op.attrs().get("strides", [1, 1])
-    ceil_mode = paddle_op.attrs().get("ceil_mode", False)
-    exclusive = paddle_op.attrs().get("exclusive")
-    adaptive = paddle_op.attrs().get("adaptive")
-    padding_algorithm = paddle_op.attrs().get("padding_algorithm")
 
-    input_shape = input_tensor.shape
+    input_shape = paddle_op.operands()[0].source().shape
+    input_dims = len(input_shape)
+
+    global_pooling = paddle_op.attrs().get("global_pooling", False)
+    pool_type = paddle_op.attrs().get("pooling_type")
+    strides = paddle_op.attrs().get("strides")
+    paddings = paddle_op.attrs().get("paddings")
+    exclusive = paddle_op.attrs().get("exclusive", True)
+    ceil_mode = paddle_op.attrs().get("ceil_mode", False)
+    adaptive = paddle_op.attrs().get("adaptive", False)
+    padding_algorithm = paddle_op.attrs().get("padding_algorithm", "EXPLICIT")
 
     # TODO attention for these codes
     if not paddle_op.attrs().get("kernel_size") and len(inputs) == 2:
@@ -45,44 +49,88 @@ def pool2d_converter(network, paddle_op, inputs):
     else:
         kernel_size = paddle_op.attrs().get("kernel_size")
 
-    if len(stride) == 0 or stride[0] is None:
-        stride = kernel_size
+    nv_pool_type = trt.PoolingType.MAX
+    reduce_operation = trt.ReduceOperation.MAX
+    if pool_type == "max":
+        nv_pool_type = trt.PoolingType.MAX
+        reduce_operation = trt.ReduceOperation.MAX
+    elif pool_type == "avg":
+        nv_pool_type = trt.PoolingType.AVERAGE
+        reduce_operation = trt.ReduceOperation.AVG
 
-    if pooling_type == "max":
-        pooling_type = trt.PoolingType.MAX
-    elif pooling_type == "avg":
-        pooling_type = trt.PoolingType.AVERAGE
-    else:
-        raise ValueError(f"Unsupported pooling type: {pooling_type}")
+    if global_pooling or adaptive:
+        paddings = [0] * len(paddings)
 
     if padding_algorithm == "VALID":
-        padding = [0, 0]
+        paddings = [0] * len(paddings)
 
-    if adaptive:
-        output_size = kernel_size
-        stride = tuple(input_shape[-2 + i] // output_size[i] for i in range(2))
-        kernel_size = tuple(
-            input_shape[-2 + i] - (output_size[i] - 1) * stride[i]
-            for i in range(2)
-        )
+    nv_paddings = trt.DimsHW(0, 0)
 
-        pool_layer = network.add_pooling_nd(
-            input_tensor, pooling_type, window_size=kernel_size
-        )
-        pool_layer.stride_nd = stride
-        if pooling_type == "max":
-            pool_layer.padding_nd = padding
+    if padding_algorithm == "SAME":
+        real_paddings = []
+        for i in range(2):
+            input_dim = input_shape[input_dims - 2 + i]
+            stride = strides[i]
+            kernel = kernel_size[i]
+            pad = max(
+                (math.ceil(float(input_dim) / stride) - 1) * stride
+                + kernel
+                - input_dim,
+                0,
+            )
+            pad_0 = int(pad // 2)
+            pad_1 = int(pad - pad_0)
+            real_paddings.extend([pad_0, pad_1])
+        paddings = real_paddings
+        nv_paddings = trt.DimsHW(paddings[0], paddings[2])
     else:
-        pool_layer = network.add_pooling(
-            input_tensor, pooling_type, window_size=kernel_size
-        )
-        pool_layer.stride = stride
-        pool_layer.padding = padding
-        if exclusive:
-            pool_layer.average_count_excludes_padding = True
-        else:
-            pool_layer.average_count_excludes_padding = False
-        if ceil_mode:
-            pool_layer.padding_mode = trt.PaddingMode.EXPLICIT_ROUND_UP
+        real_paddings = []
+        for i in range(2):
+            pad = paddings[i]
+            real_paddings.extend([pad, pad])
+        paddings = real_paddings
+        nv_paddings = trt.DimsHW(paddings[0], paddings[2])
 
-    return pool_layer.get_output(0)
+    nv_ksize = trt.DimsHW(kernel_size[0], kernel_size[1])
+    nv_strides = trt.DimsHW(strides[0], strides[1])
+
+    layer = None
+    pooling_layer = network.add_pooling_nd(
+        input=input_tensor, type=nv_pool_type, window_size=nv_ksize
+    )
+    pooling_layer.stride_nd = nv_strides
+    pooling_layer.padding_nd = nv_paddings
+    pooling_layer.average_count_excludes_padding = exclusive
+    if padding_algorithm == "SAME":
+        pooling_layer.padding_mode = trt.PaddingMode.SAME_UPPER
+    else:
+        pooling_layer.padding_mode = trt.PaddingMode.EXPLICIT_ROUND_UP
+    layer = pooling_layer
+    if not adaptive and not global_pooling and ceil_mode:
+        pooling_layer = network.add_pooling_nd(
+            input=input_tensor, type=nv_pool_type, window_size=nv_ksize
+        )
+        pooling_layer.stride_nd = nv_strides
+        pooling_layer.padding_nd = nv_paddings
+        pooling_layer.average_count_excludes_padding = exclusive
+        if padding_algorithm == "SAME":
+            pooling_layer.padding_mode = trt.PaddingMode.SAME_UPPER
+        else:
+            pooling_layer.padding_mode = trt.PaddingMode.EXPLICIT_ROUND_UP
+        layer = pooling_layer
+    elif global_pooling and not adaptive:
+        reduce_axes = (1 << (input_dims - 2)) | (1 << (input_dims - 1))
+        reduce_layer = network.add_reduce(
+            input=input_tensor,
+            op=reduce_operation,
+            axes=reduce_axes,
+            keep_dims=True,
+        )
+        layer = reduce_layer
+    else:
+        raise NotImplementedError(
+            "The combination of attributes is not supported yet."
+        )
+
+    output_tensor = layer.get_output(0)
+    return output_tensor
