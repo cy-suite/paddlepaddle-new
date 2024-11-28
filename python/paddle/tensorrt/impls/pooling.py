fa14c93753b5c835e7f04cba4ad7f6d76a15d79e
lizexu <2694294196@qq.com>
2024-11-28 06:36:09 +0000

    fix

--- python/paddle/tensorrt/impls/pooling.py
+++ python/paddle/tensorrt/impls/pooling.py
@@ -40,16 +40,13 @@ def pool2d_converter(network, paddle_op, inputs):
     adaptive = paddle_op.attrs().get("adaptive", False)
     padding_algorithm = paddle_op.attrs().get("padding_algorithm", "EXPLICIT")
 
-    # TODO attention for these codes
     if not paddle_op.attrs().get("kernel_size") and len(inputs) == 2:
-        # the size of pool2d inputs is 2, means kernel size is the second input.
-        # kernel_size_tensor = inputs[1]
         full_int_op = paddle_op.operands()[1].source().get_defining_op()
         if full_int_op.name() == "pd_op.full_int_array":
             kernel_size = full_int_op.attrs().get("value")
         else:
             raise Exception(
-                "the defining op of kernel size must be pd_op.full_int_array"
+                "The defining op of kernel size must be pd_op.full_int_array"
             )
     else:
         kernel_size = paddle_op.attrs().get("kernel_size")
@@ -100,19 +97,40 @@ def pool2d_converter(network, paddle_op, inputs):
     nv_strides = trt.DimsHW(strides[0], strides[1])
 
     layer = None
+    g_pre_pad = trt.DimsHW(0, 0)
+    g_post_pad = trt.DimsHW(0, 0)
 
-    pooling_layer = network.add_pooling_nd(
-        input=input_tensor, type=nv_pool_type, window_size=nv_ksize
-    )
-    pooling_layer.stride_nd = nv_strides
-    pooling_layer.padding_nd = nv_paddings
-    pooling_layer.average_count_excludes_padding = exclusive
-    if padding_algorithm == "SAME":
-        pooling_layer.padding_mode = trt.PaddingMode.SAME_UPPER
-    else:
-        pooling_layer.padding_mode = trt.PaddingMode.EXPLICIT_ROUND_UP
-    layer = pooling_layer
-    if not adaptive and not global_pooling and ceil_mode:
+    if (
+        input_shape[input_dims - 2] > 0
+        and input_shape[input_dims - 2] - kernel_size[0] + 2 * paddings[0] < 0
+    ):
+        g_post_pad.h = strides[0] - 1
+    if (
+        input_shape[input_dims - 1] > 0
+        and input_shape[input_dims - 1] - kernel_size[1] + 2 * paddings[1] < 0
+    ):
+        g_post_pad.w = strides[1] - 1
+
+    if not adaptive and not global_pooling and not ceil_mode:
+        if padding_algorithm != "SAME" and (
+            (g_post_pad.h > 0 and input_shape[input_dims - 2] > 0)
+            or (g_post_pad.w > 0 and input_shape[input_dims - 1] > 0)
+        ):
+            raise NotImplementedError(
+                "The combination of attributes is not supported yet."
+            )
+        pooling_layer = network.add_pooling_nd(
+            input=input_tensor, type=nv_pool_type, window_size=nv_ksize
+        )
+        pooling_layer.stride_nd = nv_strides
+        pooling_layer.padding_nd = nv_paddings
+        pooling_layer.average_count_excludes_padding = exclusive
+        if padding_algorithm == "SAME":
+            pooling_layer.padding_mode = trt.PaddingMode.SAME_UPPER
+        else:
+            pooling_layer.padding_mode = trt.PaddingMode.EXPLICIT_ROUND_UP
+        layer = pooling_layer
+    elif not adaptive and not global_pooling and ceil_mode:
         pooling_layer = network.add_pooling_nd(
             input=input_tensor, type=nv_pool_type, window_size=nv_ksize
         )
