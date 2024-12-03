fdfb21f929d3422c613d557fbeb5c48cbecd77b7
PuQing <me@puqing.work>
2024-12-03 15:50:26 +0800

    Revert "fix"
    This reverts commit 6374d3ca78d8851c81d7bf5c54a0441eb9937f77.

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/cinn_op_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/cinn_op_infer_sym.cc
@@ -346,16 +346,14 @@ bool SliceOpInferSymbolicShape(pir::Operation *op,
 
   const ExprVec starts = paddle::dialect::details::VecInt642Expr(starts_raw);
   const ExprVec ends = paddle::dialect::details::VecInt642Expr(ends_raw);
-  const ExprVec strides = std::vector<symbol::DimExpr>(starts.size(), 1);
 
   infer_context->SetShapeOrDataForValue(
       op->result(0),
-      paddle::dialect::slice_utils::StridedSliceRawInferSymbolicShape(
+      paddle::dialect::slice_utils::SliceRawInferSymbolicShape(
           op->operand_source(0),
           op->result(0),
           starts,
           ends,
-          strides,
           axes_raw,
           infer_flags_raw,
           decrease_axis_raw,
