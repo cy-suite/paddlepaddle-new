da902a734af4c7918a6d3211e6a515f053843bc2
ooooo <3164076421@qq.com>
2024-12-03 09:31:27 +0800

    refine

--- test/legacy_test/op_test.py
+++ test/legacy_test/op_test.py
@@ -1635,10 +1635,14 @@ class OpTest(unittest.TestCase):
                                     )
                                 )
                                 expect_shape = outs[i].shape
-                                if len(outs[i].shape) <= 1 and np.issubdtype(
+                                if len(outs[i].shape) == 1 and np.issubdtype(
                                     outs[i].dtype, np.integer
                                 ):
                                     expect_data = outs[i].tolist()
+                                elif len(outs[i].shape) == 0 and np.issubdtype(
+                                    outs[i].dtype, np.integer
+                                ):
+                                    expect_data = [outs[i].tolist()]
                                 else:
                                     expect_data = []
                                 i += 1
