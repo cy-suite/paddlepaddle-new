3170b7ffd308afcf2115dd012a4a2835fef7c3f1
xiaoguoguo626807 <wangruting@baidu.com>
2024-12-03 03:19:27 +0000

    modify windows ci

--- test/cpp/pir/custom_engine/custom_engine_op.h
+++ test/cpp/pir/custom_engine/custom_engine_op.h
@@ -56,4 +56,4 @@ class FakeEngineOp
 }  // namespace dialect
 }  // namespace paddle
 
-IR_EXPORT_DECLARE_EXPLICIT_TYPE_ID(paddle::dialect::FakeEngineOp)
+IR_DECLARE_EXPLICIT_TYPE_ID(paddle::dialect::FakeEngineOp)
