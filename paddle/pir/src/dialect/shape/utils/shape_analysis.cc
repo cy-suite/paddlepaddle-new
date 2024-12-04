6ec1838755a69b5ef51b94cfc9bcfcd65c3367b0
risemeup1 <515586620@qq.com>
2024-12-04 09:19:44 +0000

    fix

--- paddle/pir/src/dialect/shape/utils/shape_analysis.cc
+++ paddle/pir/src/dialect/shape/utils/shape_analysis.cc
@@ -799,7 +799,7 @@ pir::PrintHooks ShapeConstraintIRAnalysis::PrintHook() {
       }
     }
     printer.os << " }";
-    printer.os << "\t(op_" << op.id() << ")";
+    // printer.os << "\t(op_" << op.id() << ")";
   };
   return print_hook;
 }
