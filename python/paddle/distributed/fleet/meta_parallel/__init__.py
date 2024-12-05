d442d3f299c4e70f1ac28efcd00a70c0e91a8961
zhangyuqin1998 <zhangyuqin@baidu.com>
2024-12-05 11:04:48 +0800

    Supporting for memory efficient fthenb

--- python/paddle/distributed/fleet/meta_parallel/__init__.py
+++ python/paddle/distributed/fleet/meta_parallel/__init__.py
@@ -28,6 +28,7 @@ from .pipeline_parallel import (  # noqa: F401
     PipelineParallel,
     PipelineParallelWithInterleave,
     PipelineParallelWithInterleaveFthenB,
+    PipelineParallelWithInterleaveFthenBMemoryEfficient,
 )
 from .segment_parallel import SegmentParallel  # noqa: F401
 from .sharding_parallel import ShardingParallel  # noqa: F401
