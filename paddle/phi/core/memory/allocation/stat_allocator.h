ca74f1817304af61a7734654114083639a67d411
blacksheep-Aristotle <zhangweilong01@baidu.com>
2024-12-09 20:31:45 +0800

    [AutoParallel]:fix FLAGS_log_memory_stats wrong log

--- paddle/phi/core/memory/allocation/stat_allocator.h
+++ paddle/phi/core/memory/allocation/stat_allocator.h
@@ -31,14 +31,6 @@ class StatAllocator : public Allocator {
 
  protected:
   void FreeImpl(phi::Allocation* allocation) override {
-    if (phi::is_cpu_place(allocation->place()) ||
-        phi::is_cuda_pinned_place(allocation->place())) {
-      HOST_MEMORY_STAT_UPDATE(
-          Allocated, allocation->place().GetDeviceId(), -allocation->size());
-    } else {
-      DEVICE_MEMORY_STAT_UPDATE(
-          Allocated, allocation->place().GetDeviceId(), -allocation->size());
-    }
     platform::RecordMemEvent(allocation->ptr(),
                              allocation->place(),
                              allocation->size(),
