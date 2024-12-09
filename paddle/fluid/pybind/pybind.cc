0b35a94546bdef2db05d8b79b43088ab6fa8d49e
co63oc <co63oc@users.noreply.github.com>
2024-12-09 14:40:07 +0800

    Fix

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -2613,7 +2613,6 @@ All parameter, weight, gradient are variables in Paddle.
                                    "The index to set is larger than the size "
                                    "of DenseTensorArray."));
              self[i].ShareDataWith(t);
-             self[i].set_lod(t.lod());
            })
       .def(
           "append",
@@ -2689,7 +2688,6 @@ All parameter, weight, gradient are variables in Paddle.
             self.emplace_back();
             auto &lod_tensor = PADDLE_GET(phi::DenseTensor, self.back());
             lod_tensor.ShareDataWith(t);
-            lod_tensor.set_lod(t.lod());
           },
           py::arg("var"))
 
@@ -2700,7 +2698,6 @@ All parameter, weight, gradient are variables in Paddle.
             auto &lod_tensor_array = PADDLE_GET(phi::TensorArray, self.back());
             for (size_t i = 0; i < t.size(); ++i) {
               lod_tensor_array[i].ShareDataWith(t[i]);
-              lod_tensor_array[i].set_lod(t[i].lod());
             }
           },
           py::arg("var"));
