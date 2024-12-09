10d54a8bb206f0ba520315d7c1d184db9409f93d
SCUcookie <2450603126@qq.com>
2024-12-09 11:31:58 +0000

    submit

--- paddle/phi/kernels/cpu/full_kernel.cc
+++ paddle/phi/kernels/cpu/full_kernel.cc
@@ -25,6 +25,9 @@ namespace phi {
 template <typename T, typename Context, typename VType>
 void FullValue(const Context& dev_ctx, DenseTensor* tensor, VType val) {
   dev_ctx.template Alloc<T>(tensor);
+  if (tensor->numel() == 0) {
+    return;
+  }
   auto t = phi::EigenVector<T>::Flatten(*tensor);
   t.device(*dev_ctx.eigen_device()) = t.constant(static_cast<T>(val));
 }
@@ -36,6 +39,9 @@ void FullKernel(const Context& dev_ctx,
                 DataType dtype UNUSED,
                 DenseTensor* out) {
   out->Resize(common::make_ddim(shape.GetData()));
+  if (out->numel() == 0) {
+    return;  // Handle 0-size Tensor: no allocation or computation needed.
+  }
   FullValue<T>(dev_ctx, out, val.to<T>());
 }
 
@@ -45,6 +51,10 @@ void FullLikeKernel(const Context& dev_ctx,
                     const Scalar& val,
                     DataType dtype UNUSED,
                     DenseTensor* out) {
+  out->Resize(x.dims());
+  if (out->numel() == 0) {
+    return;
+  }
   if (!std::is_same<T, phi::dtype::complex<float>>::value &&
       !std::is_same<T, phi::dtype::complex<double>>::value) {
     auto value = val.to<double>();
@@ -92,6 +102,9 @@ void FullIntArrayKernel(const Context& dev_ctx,
                         DataType dtype UNUSED,
                         DenseTensor* out) {
   out->Resize(common::make_ddim({static_cast<int64_t>(shape.size())}));
+  if (out->numel() == 0) {
+    return;
+  }
   T* out_data = dev_ctx.template Alloc<T>(out);
   for (size_t i = 0; i < shape.size(); ++i) {
     int64_t val = shape[i];
