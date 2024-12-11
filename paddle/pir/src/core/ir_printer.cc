5c60a900feb325f1d0e96008f2308676c87e224c
wangruting <wangruting@baidu.com>
2024-12-11 09:50:58 +0800

    modify DPA model

--- paddle/pir/src/core/ir_printer.cc
+++ paddle/pir/src/core/ir_printer.cc
@@ -389,9 +389,6 @@ void IrPrinter::PrintOpReturnType(const Operation& op) {
 
 void IrPrinter::AddValueAlias(Value v, const std::string& alias) {
   const void* key = v.impl();
-  PADDLE_ENFORCE_EQ(aliases_.find(key),
-                    aliases_.end(),
-                    common::errors::InvalidArgument("Value already has alias"));
   aliases_[key] = alias;
 }
 
