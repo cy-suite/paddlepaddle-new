d89446ab75b77d42f09f0ed799ceb53847f14495
andsonder <changlu@keter.top>
2024-12-11 14:14:09 +0800

    update test

--- test/auto_parallel/pir/test_sharding_tensor_fusion_save_load.py
+++ test/auto_parallel/pir/test_sharding_tensor_fusion_save_load.py
@@ -50,16 +50,15 @@ class TestSimpleNetShardingTensorFusionSaveLoad(
             {
                 "backend": ["gpu"],
                 "amp": ['1'],
-                "amp_dtype": ['float16', 'bfloat16'],
-                'amp_level': ['O1', 'O2'],
+                "amp_dtype": ['bfloat16'],
+                'amp_level': ['O1'],
                 'use_master_weight': ['1'],
                 'use_master_grad': ['1'],
-                'use_promote': ['0', '1'],
             },
         )
+
         for envs in envs_list:
             # self._log_dir.name = "./log"
-            # print('env', envs, flush=1)
             ckpt_path_tmp = tempfile.TemporaryDirectory()
             envs["ckpt_path"] = ckpt_path_tmp.name
             self.run_test_case(
