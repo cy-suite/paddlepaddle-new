dae6c3f019ba4bb1b950d7b7e1fcbd8586005e02
yuanlehome <yuanlehome@163.com>
2024-12-12 12:21:30 +0000

    fix enable_profile with gpu

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -426,7 +426,9 @@ bool AnalysisPredictor::Init(
     platform::CudaProfilerStart();
     platform::NvprofEnableRecordEvent();
 #endif
-    platform::EnableProfiler(platform::ProfilerState::kAll);
+    platform::EnableProfiler(config_.use_gpu()
+                                 ? platform::ProfilerState::kAll
+                                 : platform::ProfilerState::kCPU;);
   }
 
   if (!status_is_cloned_) {
