8e3589b64b2420bc63b9efb75ec84c81f3da4891
ckl117 <ckl117@163.com>
2024-12-12 09:23:45 +0000

    check test

--- test/openvino/test_openvino_resnet50.py
+++ test/openvino/test_openvino_resnet50.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-# Copyright (c) 2021 CINN Authors. All Rights Reserved.
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,94 +12,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import time
 import unittest
 
-import numpy as np
+from openvino_test_base import OpenVINOBaseTest
 
 import paddle
-import paddle.inference as paddle_infer
-
-model_dir = os.getenv('OPENVINO_MODEL_DIR')
-print("model_dir is : ", model_dir)
-paddle.device.set_device("cpu")
+from paddle.static import InputSpec
 
 
-class TestOpenVINOResnet50Model(unittest.TestCase):
+class TestOpenVINOResnet50Model(OpenVINOBaseTest):
     def setUp(self):
-        self.model_dir = model_dir
-        self.x_shape = [1, 3, 224, 224]
-        self.target_tensor = 'save_infer_model/scale_0.tmp_0'
-        self.input_tensor = 'x'
-
-    def get_paddle_inference_result(self, model_dir, data):
-        config = paddle_infer.Config(
-            model_dir + '/inference.pdmodel', model_dir + '/inference.pdiparams'
-        )
-        config.disable_gpu()
-        config.switch_ir_optim(False)
-        self.paddle_predictor_base = paddle_infer.create_predictor(config)
-        data = paddle.Tensor(data)
-        results = self.paddle_predictor_base.run([data])
-        get_tensor = self.paddle_predictor_base.get_output_handle(
-            self.target_tensor
-        ).copy_to_cpu()
-        return get_tensor
-
-    def get_config(self):
-        config = paddle_infer.Config(
-            self.model_dir + '/inference.pdmodel',
-            self.model_dir + '/inference.pdiparams',
-        )
-        config.disable_gpu()
-        config.enable_openvino_engine(paddle_infer.PrecisionType.Float32)
-        config.set_cpu_math_library_num_threads(2)
-        cache_dir = os.path.join(self.model_dir, '__cache__')
-        config.set_optim_cache_dir(cache_dir)
-        return config
-
-    def apply_test(self):
-        first_config = self.get_config()
-        start = time.time()
-        self.paddle_predictor_openvino = paddle_infer.create_predictor(
-            first_config
-        )
-        end1 = time.time()
-        print("first load model time is: %.3f sec" % (end1 - start))
-        config = self.get_config()
-        config.use_optimized_model(True)
-        self.paddle_predictor_openvino = paddle_infer.create_predictor(config)
-        end2 = time.time()
-        print("second load opt model time is: %.3f sec" % (end2 - end1))
-
-        x_data = np.random.random(self.x_shape).astype("float32")
-        data = paddle.Tensor(x_data)
-        self.paddle_predictor_openvino.run([data])
-
-        openvino_result = self.paddle_predictor_openvino.get_output_handle(
-            self.target_tensor
-        ).copy_to_cpu()
-        target_result = self.get_paddle_inference_result(self.model_dir, x_data)
-        print("result in test_model: \n")
-        openvino_result = openvino_result.reshape(-1)
-        target_result = target_result.reshape(-1)
-        for i in range(0, min(openvino_result.shape[0], 200)):
-            if np.abs(openvino_result[i] - target_result[i]) > 1e-3:
-                print(
-                    "Error! ",
-                    i,
-                    "-th data has diff with target data:\n",
-                    openvino_result[i],
-                    " vs: ",
-                    target_result[i],
-                    ". Diff is: ",
-                    openvino_result[i] - target_result[i],
-                )
-        np.testing.assert_allclose(openvino_result, target_result, atol=1e-3)
+        model = paddle.vision.models.resnet50(True)
+        self.model_name = 'resnet50'
+        input_spec = [InputSpec(shape=[None, 3, 224, 224], name='x')]
+        self.to_static(model, input_spec)
+        self.batch_size = 1
+        self.infer_threads = 3
+        self.precision = "float32"
 
     def test_model(self):
-        self.apply_test()
+        self.check_result()
+
+
+class TestOpenVINOResnet50ModelFP16(OpenVINOBaseTest):
+    def setUp(self):
+        model = paddle.vision.models.resnet50(True)
+        self.model_name = 'resnet50'
+        input_spec = [InputSpec(shape=[None, 3, 224, 224], name='x')]
+        self.to_static(model, input_spec)
+        self.batch_size = 2
+        self.infer_threads = 3
+        self.precision = "float16"
+
+    def test_model(self):
+        self.check_result()
+
+
+class TestOpenVINOResnet50ModelBs2(OpenVINOBaseTest):
+    def setUp(self):
+        model = paddle.vision.models.resnet50(True)
+        self.model_name = 'resnet50'
+        input_spec = [InputSpec(shape=[None, 3, 224, 224], name='x')]
+        self.to_static(model, input_spec)
+        self.batch_size = 2
+        self.infer_threads = 3
+        self.precision = "float32"
+
+    def test_model(self):
+        self.check_result()
 
 
 if __name__ == "__main__":
