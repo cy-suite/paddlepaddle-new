cb94653f9dd793203af7958cb4a7a6330f36441a
andsonder <changlu@keter.top>
2024-12-13 15:25:22 +0800

    add default value for copy_tensor

--- python/paddle/distributed/auto_parallel/api.py
+++ python/paddle/distributed/auto_parallel/api.py
@@ -2784,7 +2784,7 @@ class DistModel:
                                     local_state_dict.pop(ori_p + suffix)
 
         dist_main_program.set_state_dict(
-            local_state_dict, paddle.static.global_scope(), copy_tensor
+            local_state_dict, paddle.static.global_scope()
         )
 
 
