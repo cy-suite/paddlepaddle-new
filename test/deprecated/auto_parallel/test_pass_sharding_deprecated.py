38cb6c35c1a286cf531254dc2f73b1529af04255
co63oc <co63oc@users.noreply.github.com>
2024-12-17 16:45:55 +0800

    Fix

--- test/deprecated/auto_parallel/test_pass_sharding_deprecated.py
+++ test/deprecated/auto_parallel/test_pass_sharding_deprecated.py
@@ -12,45 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import subprocess
-import sys
-import tempfile
 import unittest
-
-
-class TestShardingPass(unittest.TestCase):
-    def test_dp2sharding2(self):
-        file_dir = os.path.dirname(os.path.abspath(__file__))
-        launch_model_path = os.path.join(
-            file_dir, "sharding_pass_unittest_deprecated.py"
-        )
-
-        if os.environ.get("WITH_COVERAGE", "OFF") == "ON":
-            coverage_args = ["-m", "coverage", "run", "--branch", "-p"]
-        else:
-            coverage_args = []
-
-        tmp_dir = tempfile.TemporaryDirectory()
-        cmd = [
-            sys.executable,
-            "-u",
-            *coverage_args,
-            "-m",
-            "paddle.distributed.launch",
-            "--devices",
-            "0,1",
-            "--log_dir",
-            tmp_dir.name,
-            launch_model_path,
-        ]
-
-        process = subprocess.Popen(cmd)
-        process.wait()
-        self.assertEqual(process.returncode, 0)
-
-        tmp_dir.cleanup()
-
 
 if __name__ == "__main__":
     unittest.main()
