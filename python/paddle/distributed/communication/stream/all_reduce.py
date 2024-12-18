6d6f5a2598199a289b6d3236800cc604e021f3ec
shenfangjian <646782319@qq.com>
2024-12-18 12:10:01 +0800

    fix

--- python/paddle/distributed/communication/stream/all_reduce.py
+++ python/paddle/distributed/communication/stream/all_reduce.py
@@ -79,6 +79,7 @@ def _all_reduce_in_static_mode(
         'all_reduce',
     )
 
+    op_type = _get_reduce_op(op, "allreduce")
     ring_id = 0 if group is None else group.id
 
     if not isinstance(ring_id, int):
