84b188cbebf452c6d53ce444c3f65784335b477a
Junjie Zhang <1356732652@qq.com>
2024-12-20 10:53:21 +0800

    Update isfinite_kernel_impl.h

--- paddle/phi/kernels/impl/isfinite_kernel_impl.h
+++ paddle/phi/kernels/impl/isfinite_kernel_impl.h
@@ -39,24 +39,6 @@ struct is_other_float
                              std::is_floating_point<T>::value &&
                                  !is_float_or_double<T>::value> {};
 
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, bool>::type
-isfinite(T) {
-  return true; // Integral types are always finite
-}
-
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, bool>::type
-isnan(T) {
-  return false; // Integral types can never be NaN
-}
-
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, bool>::type
-isinf(T) {
-  return false; // Integral types can never be infinite
-}
-
 namespace phi {
 using Tensor = DenseTensor;
 
@@ -254,6 +236,19 @@ __global__ void IsfiniteCUDAKernel(
     const T* in_data,
     int num,
     bool* out_data,
+    typename std::enable_if<!std::is_floating_point<T>::value>::type* = 0) {
+  unsigned int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  for (int i = idx; i < num; i += blockDim.x * gridDim.x) {
+    const T& a = in_data[i];
+    out_data[i] = true;
+  }
+}
+
+template <typename T>
+__global__ void IsfiniteCUDAKernel(
+    const T* in_data,
+    int num,
+    bool* out_data,
     typename std::enable_if<std::is_floating_point<T>::value>::type* = 0) {
   unsigned int idx = threadIdx.x + blockIdx.x * blockDim.x;
   for (int i = idx; i < num; i += blockDim.x * gridDim.x) {
@@ -280,6 +275,19 @@ __global__ void IsnanCUDAKernel(
     const T* in_data,
     int num,
     bool* out_data,
+    typename std::enable_if<!std::is_floating_point<T>::value>::type* = 0) {
+  unsigned int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  for (int i = idx; i < num; i += blockDim.x * gridDim.x) {
+    const T& a = in_data[i];
+    out_data[i] = false;
+  }
+}
+
+template <typename T>
+__global__ void IsnanCUDAKernel(
+    const T* in_data,
+    int num,
+    bool* out_data,
     typename std::enable_if<std::is_floating_point<T>::value>::type* = 0) {
   unsigned int idx = threadIdx.x + blockIdx.x * blockDim.x;
   for (int i = idx; i < num; i += blockDim.x * gridDim.x) {
@@ -301,6 +309,19 @@ __global__ void IsnanCUDAKernel(
 }
 
 /* IsinfFunctor */
+template <typename T>
+__global__ void IsinfCUDAKernel(
+    const T* in_data,
+    int num,
+    bool* out_data,
+    typename std::enable_if<!std::is_floating_point<T>::value>::type* = 0) {
+  unsigned int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  for (int i = idx; i < num; i += blockDim.x * gridDim.x) {
+    const T& a = in_data[i];
+    out_data[i] = false;
+  }
+}
+
 template <typename T>
 __global__ void IsinfCUDAKernel(
     const T* in_data,
