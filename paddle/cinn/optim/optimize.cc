cf9d5c5d0713fde1190f5adf45792e7f019c5fc4
liuruyan <1823192871@qq.com>
2024-12-23 11:06:37 +0000

    fix head error

--- paddle/cinn/optim/optimize.cc
+++ paddle/cinn/optim/optimize.cc
@@ -25,7 +25,7 @@
 #include "paddle/cinn/optim/extern_call_process.h"
 #include "paddle/cinn/optim/fold_cinn_call_arguments.h"
 #include "paddle/cinn/optim/if_fold_pass.h"
-#include "paddle/cinn/optim/if_fusion.h"
+#include "paddle/cinn/optim/if_fusion_pass.h"
 #include "paddle/cinn/optim/insert_debug_log_callee.h"
 #include "paddle/cinn/optim/ir_simplify.h"
 #include "paddle/cinn/optim/lower_function_call_bind_vars.h"
