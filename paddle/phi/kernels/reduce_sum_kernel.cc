a019058ef9606760f49ce6d794ad23333a10e107
Junjie Zhang <1356732652@qq.com>
2024-12-23 21:16:20 +0800

    fix

--- paddle/phi/kernels/reduce_sum_kernel.cc
+++ paddle/phi/kernels/reduce_sum_kernel.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <set>
+
 #include "paddle/phi/kernels/reduce_sum_kernel.h"
 
 #include "paddle/phi/backends/all_context.h"
