f45fd1467fcf98f0540b94895ff7c534c8398ec1
Xinyi1, Li <xinyi1.li@intel.com>
2024-12-24 15:20:10 +0800

    add quantize pattern for Concat

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -675,6 +675,7 @@ const std::vector<std::string> kPirMkldnnBf16Passes{
     "cpu_bfloat16_pass",
     "cpu_bfloat16_type_placement_pass",
     "cpu_bf16_quantize_squash_pass",
+    "cpu_special_ops_bf16_pass",
 };
 
 const std::vector<std::string> kPirCpuPasses{
