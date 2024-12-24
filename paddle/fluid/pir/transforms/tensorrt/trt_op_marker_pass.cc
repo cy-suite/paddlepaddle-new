de9654628db6e66bfc8d0effa571b02aa16cb310
Junjie Zhang <1356732652@qq.com>
2024-12-24 14:45:55 +0800

    Update trt_op_marker_pass.cc

--- paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
+++ paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
@@ -263,7 +263,6 @@ class ActOpPattern : public pir::OpRewritePattern<OpType> {
 };
 using TanhOpPattern = ActOpPattern<paddle::dialect::TanhOp>;
 using CeluOpPattern = ActOpPattern<paddle::dialect::CeluOp>;
-using SoftplusOpPatten = ActOpPattern<paddle::dialect::SoftplusOp>;
 using TanhShrinkOpPattern = ActOpPattern<paddle::dialect::TanhShrinkOp>;
 using LogicalNotOpPattern = ActOpPattern<paddle::dialect::LogicalNotOp>;
 using LogicalNot_OpPattern = ActOpPattern<paddle::dialect::LogicalNot_Op>;
@@ -2214,7 +2213,6 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ps.Add(std::make_unique<ClipPattern>(context));
     ps.Add(std::make_unique<GridSampleOpPattern>(context));
     ps.Add(std::make_unique<StackOpPattern>(context));
-    ps.Add(std::make_unique<TanhOpPattern>(context));
     ps.Add(std::make_unique<TanhShrinkOpPattern>(context));
     ps.Add(std::make_unique<WherePattern>(context));
     ps.Add(std::make_unique<FullLikeOpPattern>(context));
