4bfaa98bfbf0b5e1c1f3f85143d685f4543f560c
fangfangssj <1135470306@qq.com>
2024-12-26 08:51:06 +0000

    fix

--- test/tensorrt/test_converter_activation.py
+++ test/tensorrt/test_converter_activation.py
@@ -101,6 +101,9 @@ class TestRelu6TRTPattern(TensorRTBaseTest):
     def test_trt_result(self):
         self.check_trt_result()
 
+    def test_trt_result_fp16(self):
+        self.check_trt_result(precision_mode="fp16")
+
 
 class TestTanhTRTPattern(TensorRTBaseTest):
     def setUp(self):
