323e7b234fc5eb7ae7c505b6d1a7820fb031f4c2
Qin-sx <Qin_sx@outlook.com>
2024-12-29 21:04:21 +0800

    formatted baddbmm version
    	modified:   paddle/phi/infermeta/ternary.cc
    	modified:   paddle/phi/infermeta/ternary.h
    	new file:   paddle/phi/kernels/baddbmm_grad_kernel.h
    	new file:   paddle/phi/kernels/baddbmm_kernel.h
    	new file:   paddle/phi/kernels/cpu/baddbmm_grad_kernel.cc
    	new file:   paddle/phi/kernels/cpu/baddbmm_kernel.cc
    	modified:   paddle/phi/kernels/funcs/blas/blas.h
    	modified:   paddle/phi/kernels/funcs/blas/blas_impl.cu.h
    	modified:   paddle/phi/kernels/funcs/blas/blas_impl.h
    	new file:   paddle/phi/kernels/gpu/baddbmm_grad_kernel.cu
    	new file:   paddle/phi/kernels/gpu/baddbmm_kernel.cu
    	new file:   paddle/phi/kernels/impl/baddbmm_grad_kernel_impl.h
    	new file:   paddle/phi/kernels/impl/baddbmm_kernel_impl.h
    	modified:   paddle/phi/ops/yaml/backward.yaml
    	modified:   paddle/phi/ops/yaml/ops.yaml
    	modified:   python/paddle/__init__.py
    	modified:   python/paddle/tensor/__init__.py
    	modified:   python/paddle/tensor/math.py

---
+++ paddle/phi/kernels/impl/baddbmm_kernel_impl.h
@@ -0,0 +1,179 @@
+/* Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+
+#include <type_traits>
+
+#include "glog/logging.h"
+
+#include "paddle/phi/kernels/baddbmm_kernel.h"
+#include "paddle/phi/kernels/funcs/blas/blas.h"
+#include "paddle/phi/kernels/funcs/eigen/common.h"
+#include "paddle/phi/kernels/funcs/eigen/eigen_function.h"
+namespace phi {
+
+template <typename T,
+          size_t D,
+          int MajorType = Eigen::RowMajor,
+          typename IndexType = Eigen::DenseIndex>
+using PhiEigenTensor = EigenTensor<T, D, MajorType, IndexType>;
+
+using Array1 = Eigen::DSizes<Eigen::DenseIndex, 1>;
+using Array2 = Eigen::DSizes<Eigen::DenseIndex, 2>;
+using Array3 = Eigen::DSizes<Eigen::DenseIndex, 3>;
+
+template <typename T, typename Context>
+void BaddbmmKernel(const Context& dev_ctx,
+                   const DenseTensor& input,
+                   const DenseTensor& x,
+                   const DenseTensor& y,
+                   float beta,
+                   float alpha,
+                   DenseTensor* out) {
+  auto input_dims = input.dims();
+  auto x_dims = x.dims();
+  auto y_dims = y.dims();
+
+  DenseTensor input_3d(input);
+  if (input.dims().size() == 2) {
+    input_dims = {1, input.dims()[0], input.dims()[1]};
+    input_3d.Resize(input_dims);
+  }
+
+  // broadcast mode check
+  if (x_dims[0] != input_dims[0]) {
+    PADDLE_ENFORCE_EQ(input_dims[0],
+                      1,
+                      errors::InvalidArgument(
+                          "When x_dims[0] is not equal with input_dims[0], "
+                          "input_dims[0] must be 1 but got %s",
+                          input_dims[0]));
+    PADDLE_ENFORCE_EQ(y_dims[2] == input_dims[2] || input_dims[2] == 1,
+                      true,
+                      errors::InvalidArgument(
+                          "The input tensor shape mismatch, input shape=[%s], "
+                          "x shape=[%s], y shape=[%s]",
+                          input_dims,
+                          x_dims,
+                          y_dims));
+  }
+  if (y_dims[2] != input_dims[2]) {
+    PADDLE_ENFORCE_EQ(input_dims[2],
+                      1,
+                      errors::InvalidArgument(
+                          "When y_dims[2] is not equal with input_dims[2], "
+                          "input_dims[2] must be 1 but got %s",
+                          input_dims[2]));
+    PADDLE_ENFORCE_EQ(x_dims[0] == input_dims[0] || input_dims[0] == 1,
+                      true,
+                      errors::InvalidArgument(
+                          "The input tensor shape mismatch, input shape=[%s], "
+                          "x shape=[%s], y shape=[%s]",
+                          input_dims,
+                          x_dims,
+                          y_dims));
+  }
+  PADDLE_ENFORCE_EQ(
+      x_dims[2],
+      y_dims[1],
+      errors::InvalidArgument(
+          "The input tensor X's width must be equal with matrix Y' height. "
+          "But received X's shape = [%s], Y's shape = [%s].",
+          x_dims[2],
+          y_dims[1]));
+
+  dev_ctx.template Alloc<T>(out);
+  auto blas = funcs::GetBlas<Context, T>(dev_ctx);
+
+  // calc broadcast dim
+  Array3 bcast_dims;
+  bcast_dims[0] = x_dims[0] / input_dims[0];
+  bcast_dims[1] = x_dims[1] / input_dims[1];
+  bcast_dims[2] = y_dims[2] / input_dims[2];
+  VLOG(3) << "bcast_dims=[" << bcast_dims[0] << "," << bcast_dims[1] << ","
+          << bcast_dims[2] << "]";
+
+  // broadcast using eigen
+  const DenseTensor& const_ref_input = input_3d;
+  auto eigen_input = PhiEigenTensor<T, 3>::From(const_ref_input);
+  auto eigen_out = PhiEigenTensor<T, 3>::From(*out);
+  auto& place = *dev_ctx.eigen_device();
+  funcs::EigenBroadcast<std::decay_t<decltype(place)>, T, 3>::Eval(
+      place, eigen_out, eigen_input, bcast_dims);
+
+  // special case for float16
+  if constexpr (std::is_same_v<T, phi::dtype::float16>) {
+    float t_alpha = alpha;
+    float t_beta = beta;
+    if (x_dims[0] == 1) {
+      blas.GEMM(CblasNoTrans,
+                CblasNoTrans,
+                x_dims[1],
+                y_dims[2],
+                x_dims[2],
+                t_alpha,
+                x.data<T>(),
+                y.data<T>(),
+                t_beta,
+                out->data<T>());
+    } else {
+      blas.BatchedGEMM(CblasNoTrans,
+                       CblasNoTrans,
+                       x_dims[1],
+                       y_dims[2],
+                       x_dims[2],
+                       t_alpha,
+                       x.data<T>(),
+                       y.data<T>(),
+                       t_beta,
+                       out->data<T>(),
+                       x_dims[0],
+                       x_dims[1] * x_dims[2],
+                       x_dims[2] * y_dims[2]);
+    }
+  } else {
+    T t_alpha = static_cast<T>(alpha);
+    T t_beta = static_cast<T>(beta);
+    if (x_dims[0] == 1) {
+      blas.GEMM(CblasNoTrans,
+                CblasNoTrans,
+                x_dims[1],
+                y_dims[2],
+                x_dims[2],
+                t_alpha,
+                x.data<T>(),
+                y.data<T>(),
+                t_beta,
+                out->data<T>());
+    } else {
+      blas.BatchedGEMM(CblasNoTrans,
+                       CblasNoTrans,
+                       x_dims[1],
+                       y_dims[2],
+                       x_dims[2],
+                       t_alpha,
+                       x.data<T>(),
+                       y.data<T>(),
+                       t_beta,
+                       out->data<T>(),
+                       x_dims[0],
+                       x_dims[1] * x_dims[2],
+                       x_dims[2] * y_dims[2]);
+      // x_dims[2] == y_dims[1]
+    }
+  }
+}
+
+}  // namespace phi
