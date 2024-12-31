130251d81f58cdd24ec94e62a83d577d5a12f25a
HydrogenSulfate <490868991@qq.com>
2024-12-31 17:42:53 +0800

    update forward, backward with 0-size tensor in eager/static mode

--- test/legacy_test/test_zero_size.py
+++ test/legacy_test/test_zero_size.py
@@ -49,36 +49,259 @@ class TestZeroSizeParameter(unittest.TestCase):
                 self.assertEqual(
                     model.w.shape,
                     zero_size_shape,
-                    msg=f"error occurs at: {parameter_dtype}, {zero_size_shape}",
+                    msg=f"Check failed at: {parameter_dtype}, {zero_size_shape}",
                 )
                 self.assertEqual(
                     model.w.data_ptr(),
                     0,
-                    msg=f"error occurs at: {parameter_dtype}, {zero_size_shape}",
+                    msg=f"Check failed at: {parameter_dtype}, {zero_size_shape}",
                 )
                 self.assertEqual(
                     str(model.w.place),
                     str(model.dummy_linear.weight.place),
-                    msg=f"error occurs at: {parameter_dtype}, {zero_size_shape}",
+                    msg=f"Check failed at: {parameter_dtype}, {zero_size_shape}",
                 )
                 self.assertEqual(
                     model.w.strides,
                     zero_size_shape,
-                    msg=f"error occurs at: {parameter_dtype}, {zero_size_shape}",
+                    msg=f"Check failed at: {parameter_dtype}, {zero_size_shape}",
                 )
                 self.assertEqual(
                     model.w.is_contiguous(),
                     True,
-                    msg=f"error occurs at: {parameter_dtype}, {zero_size_shape}",
+                    msg=f"Check failed at: {parameter_dtype}, {zero_size_shape}",
                 )
 
 
 class TestZeroSizeForward(unittest.TestCase):
+    def setUp(self):
+        self.dtypes = [
+            'bool',
+            'uint8',
+            'int8',
+            'int16',
+            'int32',
+            'int64',
+            'float16',
+            'float32',
+            'float64',
+            'complex64',
+            'complex128',
+        ]
+        self.zero_size_shapes = [
+            [0, 4],
+            [0, 0],
+            [4, 0],
+            [0, 5, 6],
+            [6, 5, 0, 0],
+            [0, 0, 0, 12],
+        ]
+
     def test_forward_eager(self):
-        pass
+        """Test for single API"""
+        for dtype in self.dtypes:
+            for zero_size_shape in self.zero_size_shapes:
+                x = paddle.ones(zero_size_shape, dtype=dtype)
+                self.assertEqual(x.data_ptr(), 0)
+
+                if x.dtype == paddle.bool:
+                    y = ~x
+                else:
+                    y = x + 1
+
+                self.assertEqual(
+                    y.shape,
+                    zero_size_shape,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    y.data_ptr(),
+                    0,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    y.strides,
+                    zero_size_shape,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    str(y.place),
+                    str(x.place),
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    y.dtype,
+                    x.dtype,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    y.is_contiguous(),
+                    x.is_contiguous(),
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+
+    def test_forward_static(self):
+        """Test for single API"""
+
+        def forward_func(x):
+            if x.dtype == paddle.bool:
+                y = ~x
+            else:
+                y = x + 1
+            return y
+
+        static_forward_func = paddle.jit.to_static(
+            forward_func, full_graph=True
+        )
+
+        for dtype in self.dtypes:
+            for zero_size_shape in self.zero_size_shapes:
+                x = paddle.ones(zero_size_shape, dtype=dtype)
+                self.assertEqual(x.data_ptr(), 0)
+                y = static_forward_func(x)
+
+                self.assertEqual(
+                    y.shape,
+                    zero_size_shape,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    y.data_ptr(),
+                    0,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    y.strides,
+                    zero_size_shape,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    str(y.place),
+                    str(x.place),
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    y.dtype,
+                    x.dtype,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    y.is_contiguous(),
+                    x.is_contiguous(),
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
 
 
-class TestZeroSizeBackward(unittest.TestCase): ...
+class TestZeroSizeBackward(unittest.TestCase):
+    def setUp(self):
+        # Only floating and complex needs gradient
+        self.dtypes = [
+            'float16',
+            'float32',
+            'float64',
+            'complex64',
+            'complex128',
+        ]
+        self.zero_size_shapes = [
+            [0, 4],
+            [0, 0],
+            [4, 0],
+            [0, 5, 6],
+            [6, 5, 0, 0],
+            [0, 0, 0, 12],
+        ]
+
+    def test_backward_eager(self):
+        """Test for single API"""
+        for dtype in self.dtypes:
+            for zero_size_shape in self.zero_size_shapes:
+                x = paddle.ones(zero_size_shape, dtype=dtype)
+                x.stop_gradient = False
+                self.assertEqual(x.data_ptr(), 0)
+
+                y = x * 2 + 1
+
+                (x_grad,) = paddle.grad(y, x, create_graph=True)
+
+                self.assertEqual(
+                    x_grad.shape,
+                    zero_size_shape,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    x_grad.data_ptr(),
+                    0,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    x_grad.strides,
+                    zero_size_shape,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    str(x_grad.place),
+                    str(x.place),
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    x_grad.dtype,
+                    x.dtype,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    x_grad.is_contiguous(),
+                    x.is_contiguous(),
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+
+    def test_backward_static(self):
+        """Test for single API"""
+
+        def gradient_func(x):
+            y = x * 2 + 1
+            return paddle.grad(y, x)
+
+        for dtype in self.dtypes:
+            for zero_size_shape in self.zero_size_shapes:
+                x = paddle.ones(zero_size_shape, dtype=dtype)
+                x.stop_gradient = False
+                self.assertEqual(x.data_ptr(), 0)
+
+                static_gradient_func = paddle.jit.to_static(
+                    gradient_func, full_graph=True
+                )
+                (x_grad,) = static_gradient_func(x)
+
+                self.assertEqual(
+                    x_grad.shape,
+                    zero_size_shape,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    x_grad.data_ptr(),
+                    0,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    x_grad.strides,
+                    zero_size_shape,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    str(x_grad.place),
+                    str(x.place),
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    x_grad.dtype,
+                    x.dtype,
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
+                self.assertEqual(
+                    x_grad.is_contiguous(),
+                    x.is_contiguous(),
+                    msg=f"Check failed at: {dtype}, {zero_size_shape}",
+                )
 
 
 if __name__ == '__main__':
