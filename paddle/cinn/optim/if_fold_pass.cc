446579f6391ffc84874d7c7714189a605ce4232b
liuruyan <1823192871@qq.com>
2024-12-31 06:06:05 +0000

    fix comment

--- paddle/cinn/optim/if_fold_pass.cc
+++ paddle/cinn/optim/if_fold_pass.cc
@@ -27,6 +27,11 @@ namespace optim {
 using ir::stmt::IfThenElse;
 using ir::stmt::StmtRef;
 
+// Determine whether `IfThenElse` satisfies the following conditions:
+// 1. The condition is an equality comparison
+// 2. The right side of the equality comparison is `0`
+// 3. There are no statements in the false branch
+// 4. Only one statement in the true branch
 bool JudgeIfStmt(const StmtRef& stmt) {
   if (!stmt.isa<IfThenElse>()) return false;
   auto if_stmt = stmt.as<IfThenElse>();
@@ -39,7 +44,8 @@ bool JudgeIfStmt(const StmtRef& stmt) {
   return true;
 }
 
-bool IsInnerIfWithEqCond(const StmtRef& stmt) {
+// Only judge condition of `IfThenElse` like `xxx == 0`
+bool IsIfWithEqCond(const StmtRef& stmt) {
   if (stmt.isa<IfThenElse>()) {
     auto if_stmt = stmt.as<IfThenElse>();
     if (auto eq = if_stmt->condition().As<ir::EQ>()) {
@@ -50,20 +56,24 @@ bool IsInnerIfWithEqCond(const StmtRef& stmt) {
   }
   return false;
 }
+
 void AppendContinuousIfCond(const StmtRef& stmt,
                             std::vector<ir::IndexExpr>* cond_vec,
                             StmtRef* inner_op) {
   if (!JudgeIfStmt(stmt)) {
-    if (IsInnerIfWithEqCond(stmt)) {
+    // inner op is a `IfThenElse`, so we need to check its condition.
+    if (IsIfWithEqCond(stmt)) {
       auto eq_lhs = stmt.as<IfThenElse>()->condition().As<ir::EQ>()->a();
       if (eq_lhs.is_index())
         cond_vec->push_back(common::ChangeSeqOfDivMod(
             ir::ir_utils::IRCopy(eq_lhs).as_index().Normalize()));
     }
+    // inner op is other op.
     *inner_op = stmt;
     return;
   }
 
+  // Continuous `IfThenElse`, so we push its condition and recursively.
   auto if_stmt = stmt.as<IfThenElse>();
   auto eq_lhs = if_stmt->condition().As<ir::EQ>()->a();
   if (eq_lhs.is_index())
@@ -87,7 +97,19 @@ LogicalResult IfFoldPass::Run(StmtRef stmt) {
   VLOG(6) << "-------------cond_vec start--------------";
   for (auto v : cond_vec) {
     VLOG(6) << "v: " << v;
+    // record min length of all conditions, because we want the simplified
+    // result to be shorter
     min_len = std::min(v.length(), min_len);
+    // For all normalized conditions, they have the form `a % b /c`，modulo
+    // first and then divided. We transform it as follows:
+    // origin:
+    // ((256*j)+((1024*i)+k))/3136==0
+    // ((256*j)+((1024*i)+k))%3136/56==0
+    // ((256*j)+((1024*i)+k))%56==0
+    // Mul and Sum:
+    // ((256*j)+((1024*i)+k))/3136*3136
+    // +((256*j)+((1024*i)+k))%3136/56*56
+    // +((256*j)+((1024*i)+k))%56==0
     if (v.node_type() == ir::IrNodeTy::Div) {
       expr = expr + v * v.operand(1);
     } else {
@@ -96,6 +118,7 @@ LogicalResult IfFoldPass::Run(StmtRef stmt) {
   }
   VLOG(6) << "-------------cond_vec end----------------";
 
+  // Normalize expr to simplify the expr after Mul and Sum.
   expr = expr.Normalize(ir::IndexExpr::OptLevel::Level2);
 
   if (expr != ir::IndexExpr(0) && expr.length() < min_len &&
@@ -103,7 +126,7 @@ LogicalResult IfFoldPass::Run(StmtRef stmt) {
     VLOG(6) << "old stmt: " << stmt;
     auto stmt_if = stmt.as<IfThenElse>();
     stmt_if->set_condition(ir::EQ::Make(expr, ir::IndexExpr(0)));
-    if (IsInnerIfWithEqCond(inner_op)) {
+    if (IsIfWithEqCond(inner_op)) {
       stmt_if->set_true_case(inner_op.as<IfThenElse>()->true_case());
       stmt_if->set_false_case(inner_op.as<IfThenElse>()->false_case());
     } else {
