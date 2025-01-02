a271609740d8c18fec5baf5bd9bd99c7c89a4959
drryanhuang <zihaohuang@aliyun.com>
2025-01-02 02:23:24 +0000

    modify comments

--- paddle/phi/infermeta/unary.cc
+++ paddle/phi/infermeta/unary.cc
@@ -1832,22 +1832,20 @@ void FoldInferMeta(const MetaTensor& x,
                         "but received strides_height: %d strides_width: %d.",
                         strides[0],
                         strides[1]));
-  // check dilations
-  PADDLE_ENFORCE_GE(
-      output_height,
-      1,
-      common::errors::InvalidArgument(
-          "The `output_height` should be greater than or equal to one, "
-          "but received output_height: %d .",
-          output_height));
-  PADDLE_ENFORCE_GE(
-      output_width,
-      1,
-      common::errors::InvalidArgument(
-          "The `output_width` should be greater than or equal to one, "
-          "but received output_width: %d .",
-          output_width));
   // check output size
+  PADDLE_ENFORCE_GT(output_height,
+                    0,
+                    common::errors::InvalidArgument(
+                        "The `output_height` should be greater than zero, "
+                        "but received output_height: %d .",
+                        output_height));
+  PADDLE_ENFORCE_GT(output_width,
+                    0,
+                    common::errors::InvalidArgument(
+                        "The `output_width` should be greater than zero, "
+                        "but received output_width: %d .",
+                        output_width));
+  // check dilations
   PADDLE_ENFORCE_GT(
       dilation_height,
       0,
