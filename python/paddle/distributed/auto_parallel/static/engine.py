764e551709db6de4138afc32752ebc2d456fbf33
blacksheep-Aristotle <zhangweilong01@baidu.com>
2025-01-02 10:27:07 +0800

    [AutoParallel]: intermedaite api support lora

--- python/paddle/distributed/auto_parallel/static/engine.py
+++ python/paddle/distributed/auto_parallel/static/engine.py
@@ -343,24 +343,18 @@ class Engine:
             batch_sampler.set_epoch(0)
 
         if isinstance(data, dict):
-            data = list(data.values())
-            if len(data) == 2:
-                inputs, labels = data
-            elif len(data) == 3:
-                inputs, labels, _ = data
-            else:
+            data = tuple(data.values())
+            if len(data) != 2:
                 raise ValueError(
-                    f"Data can contain up to three keys : inputs,labels,attention_mask and at least contain inputs and labels , but received {len(data)}."
+                    f"Data should be a dict with two keys, but received {len(data)}."
                 )
+            inputs, labels = data
         elif isinstance(data, (list, tuple)):
-            if len(data) == 2:
-                inputs, labels = data
-            elif len(data) == 3:
-                inputs, labels, _ = data
-            else:
+            if len(data) != 2:
                 raise ValueError(
-                    f"Data can contain up to three keys : inputs,labels,attention_mask and at least contain inputs and labels , but received {len(data)}."
+                    f"Data should be a list or tuple with two elements, but received {len(data)}."
                 )
+            inputs, labels = data
         else:
             raise TypeError(
                 f"Data should be a dict or list, but received {type(data)}."
