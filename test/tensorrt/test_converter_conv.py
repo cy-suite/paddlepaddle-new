bd94cd5296deec86b19126f4b4af77fe2f234c32
Junjie Zhang <1356732652@qq.com>
2025-01-07 00:03:14 +0800

    add_conv3d

--- test/tensorrt/test_converter_conv.py
+++ test/tensorrt/test_converter_conv.py
@@ -301,5 +301,238 @@ class TestDepthwiseConv2dTransposeValidTRTPattern(TensorRTBaseTest):
         self.check_trt_result()
 
 
+def conv3d_wrapper(x):
+    conv = paddle.nn.Conv3D(3, 3, (3, 3, 3))
+    return conv(x)
+
+
+def conv3d_python_api(x, padding="SAME", stride=(1, 1, 1)):
+    conv = paddle.nn.Conv3D(3, 3, (3, 3, 3), padding=padding, stride=stride)
+    return conv(x)
+
+
+def conv3d_python_error_api(x, padding):
+    conv = paddle.nn.Conv3D(3, 3, (3, 3, 3), padding="SAME", stride=(1, 1, 1))
+    return conv(x)
+
+
+class TestConv3dTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_wrapper
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result_fp32(self):
+        self.check_trt_result()
+
+    def test_trt_result_fp16(self):
+        self.check_trt_result(precision_mode="fp16")
+
+
+class TestConv3dPaddingAlgorithmTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_python_api
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "padding": "SAME",
+            "stride": (1, 1, 1),
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result_fp32(self):
+        self.check_trt_result()
+
+    def test_trt_result_fp16(self):
+        self.check_trt_result(precision_mode="fp16")
+
+
+class TestConv3dPaddingSizeTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_python_error_api
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "padding": [1, 1, 1, 1],
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result(self):
+        self.check_marker(expected_result=False)
+
+
+class TestConv3dNegativePaddingTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_python_error_api
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "padding": [-1, 1, 1],
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result(self):
+        self.check_marker(expected_result=False)
+
+
+class TestConv3dZeroPaddingDimensionTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_python_api
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "padding": [0, 0, 0],
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result(self):
+        self.check_marker(expected_result=False)
+
+
+def depthwise_conv3d_transpose_wrapper(x):
+    conv = paddle.nn.Conv3DTranspose(in_channels=2, out_channels=2, kernel_size=(3, 3, 3), groups=2)
+    return conv(x)
+
+
+def depthwise_conv3d_transpose_python_api(
+    x, padding="SAME", stride=(1, 1, 1), dilation=(1, 1, 1)
+):
+    conv = paddle.nn.Conv3DTranspose(in_channels=2, out_channels=2, kernel_size=(3, 3, 3), groups=2)
+    return conv(x)
+
+
+def depthwise_conv3d_transpose_wrapper_outpadding(x, output_padding):
+    conv = paddle.nn.Conv3DTranspose(in_channels=3, out_channels=3, kernel_size=(3, 3, 3), stride=2, output_padding=output_padding)
+    return conv(x)
+
+
+def conv3d_transpose_with_error(x, padding=None):
+    conv = paddle.nn.Conv3DTranspose(
+        in_channels=3,
+        out_channels=3,
+        kernel_size=(3, 3, 3),
+    )
+    return conv(x)
+
+
+class TestDepthwiseConv3dTransposeTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = depthwise_conv3d_transpose_wrapper
+        self.api_args = {"x": np.random.random([3, 2, 8, 8, 8]).astype("float32")}
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 2, 8, 8, 8]}
+        self.max_shape = {"x": [10, 2, 8, 8, 8]}
+
+    def test_trt_result_fp32(self):
+        self.check_trt_result()
+
+    def test_trt_result_fp16(self):
+        self.check_trt_result(precision_mode="fp16")
+
+
+class TestDepthwiseConv3dTransposeSameTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_transpose_with_error
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "padding_algorithm": "SAME",
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result_fp32(self):
+        self.check_trt_result()
+
+    def test_trt_result_fp16(self):
+        self.check_trt_result(precision_mode="fp16")
+
+
+class TestDepthwiseConv3dTransposeOutputPaddingTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = depthwise_conv3d_transpose_wrapper_outpadding
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "output_padding": [1, 1, 1],
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result(self):
+        with self.assertRaises(ValueError) as context:
+            self.check_trt_result()
+
+
+class TestDepthwiseConv3dTransposeOutputPadding2TRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = depthwise_conv3d_transpose_wrapper_outpadding
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "output_padding": [0, 0, 0],
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result_fp32(self):
+        self.check_trt_result()
+
+    def test_trt_result_fp16(self):
+        self.check_trt_result(precision_mode="fp16")
+
+
+class TestConv3dTransposePaddingSizeTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_transpose_with_error
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "padding": [1, 1, 1, 1],
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result(self):
+        self.check_marker(expected_result=False)
+
+
+class TestConv3dTransposeNoDilationsTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_transpose_with_error
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result(self):
+        self.check_marker(expected_result=False)
+
+
+class TestConv3dTransposeInvalidDilationsTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = conv3d_transpose_with_error
+        self.api_args = {
+            "x": np.random.random([2, 3, 8, 8, 8]).astype("float32"),
+            "dilation": [2, 1, 1],
+        }
+        self.program_config = {"feed_list": ["x"]}
+        self.min_shape = {"x": [1, 3, 8, 8, 8]}
+        self.max_shape = {"x": [10, 3, 8, 8, 8]}
+
+    def test_trt_result(self):
+        self.check_marker(expected_result=False)
+
+
 if __name__ == '__main__':
     unittest.main()
