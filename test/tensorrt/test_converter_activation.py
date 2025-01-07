1fbaf93d50bc4b9140d46701dc97f9a5b07d8f05
lizexu <2694294196@qq.com>
2025-01-07 03:34:58 +0000

    fix

--- test/tensorrt/test_converter_activation.py
+++ test/tensorrt/test_converter_activation.py
@@ -225,6 +225,7 @@ class TestTanhShrinkOpFloatTRTPattern(TensorRTBaseTest):
         }
         self.program_config = {"feed_list": ["x"]}
         self.min_shape = {"x": [1, 3]}
+        self.opt_shape = {"x": [2, 3]}
         self.max_shape = {"x": [5, 3]}
 
     def test_trt_result_fp16(self):
