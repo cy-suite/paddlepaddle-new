8cf03aefbefa4ee1152853f03d88f8454194b79c
Junjie Zhang <1356732652@qq.com>
2025-01-10 14:54:14 +0800

    Update trt_op_marker_pass.cc

--- paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
+++ paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
@@ -987,20 +987,21 @@ class PadOpPattern : public pir::OpRewritePattern<paddle::dialect::PadOp> {
       return false;
     }
     float pad_value = 0.0f;
-    if(op->HasAttribute("pad_value")){
+    if (op->HasAttribute("pad_value")) {
       auto pad_value = op->attribute<pir::FloatAttribute>("pad_value").data();
     }
     if (pir::GetDefiningOpForInput(op, 1)->isa<paddle::dialect::FullOp>()) {
       paddle::dialect::FullOp full_op =
           pir::GetDefiningOpForInput(op, 1)
               ->dyn_cast<paddle::dialect::FullOp>();
-      auto pad_value = full_op->attribute<paddle::dialect::ScalarAttribute>("value")
-                      .data()
-                      .to<float>();
+      auto pad_value =
+          full_op->attribute<paddle::dialect::ScalarAttribute>("value")
+              .data()
+              .to<float>();
     }
     if (pad_value != 0.0f) {
-        VLOG(3) << "The pad layer of TRT only support zero.";
-        return false;
+      VLOG(3) << "The pad layer of TRT only support zero.";
+      return false;
     }
     auto paddings_attr = op->attribute<pir::ArrayAttribute>("paddings");
     std::vector<int> paddings;
