e50862bc7db70cb0efc00df126fce276ea87a726
xiaoguoguo626807 <wangruting@baidu.com>
2025-01-13 10:46:07 +0000

    add extention

--- paddle/fluid/pir/utils/sub_graph_detector.cc
+++ paddle/fluid/pir/utils/sub_graph_detector.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/pir/transforms/sub_graph_detector.h"
+#include "paddle/fluid/pir/utils/sub_graph_detector.h"
 
 #include <memory>
 
