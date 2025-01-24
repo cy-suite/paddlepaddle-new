9083f4b92c62ced61611ce73164adba13222c368
Nyakku Shigure <sigure.qaq@gmail.com>
2025-01-24 10:44:35 +0800

    Update test/deprecated/legacy_test/test_gradient_clip_deprecated.py

--- test/deprecated/legacy_test/test_gradient_clip_deprecated.py
+++ test/deprecated/legacy_test/test_gradient_clip_deprecated.py
@@ -361,7 +361,7 @@ class TestGradientClipByGlobalNorm(TestGradientClip):
         for place in self.get_places():
             self.check_sparse_gradient_clip(place)
 
-    # raise typeError
+    # raise TypeError
     def test_type_error(self):
         # the type of optimizer(grad_clip=) must be an instance of GradientClipBase's derived class
         with self.assertRaises(TypeError):
