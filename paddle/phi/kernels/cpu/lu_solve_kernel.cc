0c8bc7ffda6cdb4d245c2497e172e5f6052b6976
decade-afk <3995409050@qq.com>
2025-01-26 14:55:41 +0800

    fix

--- paddle/phi/kernels/cpu/lu_solve_kernel.cc
+++ paddle/phi/kernels/cpu/lu_solve_kernel.cc
@@ -13,8 +13,8 @@
 // limitations under the License.
 
 #include "paddle/phi/backends/cpu/cpu_context.h"
-#include "paddle/phi/backends/dynload/lapack.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/funcs/lapack/lapack_function.h"
 
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/enforce.h"
