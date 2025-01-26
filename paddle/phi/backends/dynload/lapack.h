c5a8e668631e89dbbe5bb7a45ba4ce33a8ca0fca
decade-afk <3995409050@qq.com>
2025-01-26 11:04:42 +0800

    update

--- paddle/phi/backends/dynload/lapack.h
+++ paddle/phi/backends/dynload/lapack.h
@@ -48,24 +48,6 @@ extern "C" void dgetrs_(char *trans,
                         double *b,
                         int *ldb,
                         int *info);
-extern "C" void cgetrs_(char *trans,
-                        int *n,
-                        int *nrhs,
-                        std::complex<float> *a,
-                        int *lda,
-                        int *ipiv,
-                        std::complex<float> *b,
-                        int *ldb,
-                        int *info);
-extern "C" void zgetrs_(char *trans,
-                        int *n,
-                        int *nrhs,
-                        std::complex<double> *a,
-                        int *lda,
-                        int *ipiv,
-                        std::complex<double> *b,
-                        int *ldb,
-                        int *info);
 
 // evd
 extern "C" void zheevd_(char *jobz,
