c579b0a7ee906f813b8b41a3366b47ddb1e25a8e
decade-afk <3995409050@qq.com>
2025-01-26 23:33:19 +0800

    test

--- paddle/phi/kernels/gpu/lu_solve_grad_kernel.cu
+++ paddle/phi/kernels/gpu/lu_solve_grad_kernel.cu
@@ -1,41 +1,41 @@
-// Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// // Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
 
-#include "paddle/phi/backends/gpu/gpu_context.h"
-#include "paddle/phi/core/kernel_registry.h"
+// #include "paddle/phi/backends/gpu/gpu_context.h"
+// #include "paddle/phi/core/kernel_registry.h"
 
-#include "paddle/phi/kernels/lu_solve_grad_kernel.h"
-#include "paddle/phi/kernels/lu_solve_kernel.h"
+// #include "paddle/phi/kernels/lu_solve_grad_kernel.h"
+// #include "paddle/phi/kernels/lu_solve_kernel.h"
 
-namespace phi {
+// namespace phi {
 
-template <typename T, typename Context>
-void LuSolveGradKernel(const Context& dev_ctx,
-                       const DenseTensor& x,
-                       const DenseTensor& lu,
-                       const DenseTensor& pivots,
-                       const DenseTensor& out_grad,
-                       const std::string& trans,
-                       DenseTensor* x_grad) {
-  // Allocate memory for x_grad
-  dev_ctx.template Alloc<T>(x_grad);
+// template <typename T, typename Context>
+// void LuSolveGradKernel(const Context& dev_ctx,
+//                        const DenseTensor& x,
+//                        const DenseTensor& lu,
+//                        const DenseTensor& pivots,
+//                        const DenseTensor& out_grad,
+//                        const std::string& trans,
+//                        DenseTensor* x_grad) {
+//   // Allocate memory for x_grad
+//   dev_ctx.template Alloc<T>(x_grad);
 
-  // Use the forward kernel to compute the gradient
-  LuSolveKernel<T, Context>(dev_ctx, out_grad, lu, pivots, trans, x_grad);
-}
+//   // Use the forward kernel to compute the gradient
+//   LuSolveKernel<T, Context>(dev_ctx, out_grad, lu, pivots, trans, x_grad);
+// }
 
-}  // namespace phi
+// }  // namespace phi
 
-PD_REGISTER_KERNEL(
-    lu_solve_grad, GPU, ALL_LAYOUT, phi::LuSolveGradKernel, float, double) {}
+// PD_REGISTER_KERNEL(
+//     lu_solve_grad, GPU, ALL_LAYOUT, phi::LuSolveGradKernel, float, double) {}
