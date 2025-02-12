2e20d1bcb9a2249edf0195b3a6c7fdeab3dfd29e
gushiwei <gushiwei@baidu.com>
2025-02-12 06:52:55 +0000

    test=document_fix

---
+++ paddle/scripts/paddle_build/download_paddle.sh
@@ -0,0 +1,21 @@
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+rm -rf * .[^.]*
+wget -q --no-proxy https://paddle-github-action.bj.bcebos.com/PR/Paddle/$1/$2/Paddle.tar.gz --no-check-certificate
+tar xf Paddle.tar.gz --strip-components=1
+rm Paddle.tar.gz
+git config --global user.name "PaddleCI"
+git config --global user.email "paddle_ci@example.com"
+git remote add upstream https://github.com/PaddlePaddle/Paddle.git
