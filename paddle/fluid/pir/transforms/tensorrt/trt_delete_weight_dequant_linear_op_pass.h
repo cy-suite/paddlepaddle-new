75dfb4a7e769b676b9e3826405e9422c4c30c06c
YuanRisheng <yuanrisheng@baidu.com>
2025-02-13 12:49:16 +0000

    support int8 quant in trt

--- paddle/fluid/pir/transforms/tensorrt/trt_delete_weight_dequant_linear_op_pass.h
+++ paddle/fluid/pir/transforms/tensorrt/trt_delete_weight_dequant_linear_op_pass.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 #pragma once
 
 #include <glog/logging.h>
+#include <memory>
 #include "paddle/pir/include/core/dll_decl.h"
 
 namespace pir {
