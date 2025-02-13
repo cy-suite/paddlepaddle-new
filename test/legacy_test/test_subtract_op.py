1b75520b31eb42be081533eded70b3d399180457
fangfangssj <1135470306@qq.com>
2025-02-13 09:41:54 +0000

    fix

--- test/legacy_test/test_subtract_op.py
+++ test/legacy_test/test_subtract_op.py
@@ -12,103 +12,110 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import itertools
 import unittest
 
 import numpy as np
-from utils import dygraph_guard, static_guard
 
 import paddle
-from paddle import base, static
+from paddle.base import core
 
 
-class TestSubtractAPI(unittest.TestCase):
+class ApiSubtractTest(unittest.TestCase):
     def setUp(self):
-        self.places = []
-        self.places.append(paddle.CPUPlace())
-        if paddle.is_compiled_with_cuda():
-            self.places.append(paddle.CUDAPlace(0))
-
-        self.dtypes = [
-            'int16',
-            'int32',
-            'int64',
-            'float32',
-            'float64',
-            'complex64',
-            'complex128',
-        ]
-        self.shapes = [
-            ((), ()),
-            ((), (3,)),
-            ((0,), (0,)),
-            ((3,), (3,)),
-            ((1,), (3,)),
-            ((5,), (1,)),
-            ((0,), ()),
-            ((0, 3), (3,)),
-            ((3, 1), (3, 4)),
-            ((10, 15), (10, 15)),
-            ((2, 0), (1, 0)),
-            ((3, 4, 5), ()),
-            ((1, 1, 1), (3, 4, 5)),
-            ((1, 3, 5), (4, 1, 5)),
-            ((0, 3, 1), (1, 3, 5)),
-            ((1, 0, 2), (3, 0, 1)),
-        ]
-
-    def generate_random_data(self, shape, dtype):
-        if np.issubdtype(np.dtype(dtype), np.integer):
-            return np.random.randint(0, 100, size=shape).astype(dtype)
-        elif np.issubdtype(np.dtype(dtype), np.floating):
-            return np.random.uniform(-100, 100, size=shape).astype(dtype)
-        elif np.issubdtype(np.dtype(dtype), np.complexfloating):
-            real_part = np.random.uniform(-100, 100, size=shape)
-            imag_part = np.random.uniform(-100, 100, size=shape)
-            return (real_part + 1j * imag_part).astype(dtype)
+        if core.is_compiled_with_cuda():
+            self.place = core.CUDAPlace(0)
         else:
-            raise ValueError(f"Unsupported dtype: {dtype}")
+            self.place = core.CPUPlace()
 
-    def test_dygraph(self):
-        with dygraph_guard():
-            for shape, dtype in itertools.product(self.shapes, self.dtypes):
-                x_np = self.generate_random_data(shape[0], dtype)
-                y_np = self.generate_random_data(shape[1], dtype)
-                expected = np.subtract(x_np, y_np)
-                for place in self.places:
-                    x = paddle.to_tensor(x_np, dtype=dtype, place=place)
-                    y = paddle.to_tensor(y_np, dtype=dtype, place=place)
-                    result = paddle.subtract(x, y)
-                    np.testing.assert_allclose(
-                        result, expected, rtol=1e-05, atol=1e-05
-                    )
+        self.input_x = np.random.rand(10, 15).astype("float32")
+        self.input_y = np.random.rand(10, 15).astype("float32")
+        self.input_z = np.random.rand(15).astype("float32")
+        self.input_a = np.array([0, np.nan, np.nan]).astype('int64')
+        self.input_b = np.array([2, np.inf, -np.inf]).astype('int64')
+        self.input_c = np.array([4, 1, 3]).astype('int64')
 
-    def test_static(self):
-        with static_guard():
-            for shape, dtype in itertools.product(self.shapes, self.dtypes):
-                x_np = self.generate_random_data(shape[0], dtype)
-                y_np = self.generate_random_data(shape[1], dtype)
-                expected = np.subtract(x_np, y_np)
-                for place in self.places:
-                    with static.program_guard(
-                        static.Program(), static.Program()
-                    ):
-                        x = paddle.static.data(
-                            name="x", shape=shape[0], dtype=dtype
-                        )
-                        y = paddle.static.data(
-                            name="y", shape=shape[1], dtype=dtype
-                        )
-                        result = paddle.subtract(x, y)
-                        exe = base.Executor(place=place)
-                        fetches = exe.run(
-                            feed={"x": x_np, "y": y_np},
-                            fetch_list=[result],
-                        )
-                        np.testing.assert_allclose(
-                            fetches[0], expected, rtol=1e-05, atol=1e-05
-                        )
+        self.np_expected1 = np.subtract(self.input_x, self.input_y)
+        self.np_expected2 = np.subtract(self.input_x, self.input_z)
+        self.np_expected3 = np.subtract(self.input_a, self.input_c)
+        self.np_expected4 = np.subtract(self.input_b, self.input_c)
 
+    def test_static_api(self):
+        paddle.enable_static()
+        with paddle.static.program_guard(
+            paddle.static.Program(), paddle.static.Program()
+        ):
+            data_x = paddle.static.data("x", shape=[10, 15], dtype="float32")
+            data_y = paddle.static.data("y", shape=[10, 15], dtype="float32")
+            result_max = paddle.subtract(data_x, data_y)
+            exe = paddle.static.Executor(self.place)
+            (res,) = exe.run(
+                feed={"x": self.input_x, "y": self.input_y},
+                fetch_list=[result_max],
+            )
+        np.testing.assert_allclose(res, self.np_expected1, rtol=1e-05)
 
-if __name__ == '__main__':
-    unittest.main()
+        with paddle.static.program_guard(
+            paddle.static.Program(), paddle.static.Program()
+        ):
+            data_x = paddle.static.data("x", shape=[10, 15], dtype="float32")
+            data_z = paddle.static.data("z", shape=[15], dtype="float32")
+            result_max = paddle.subtract(data_x, data_z)
+            exe = paddle.static.Executor(self.place)
+            (res,) = exe.run(
+                feed={"x": self.input_x, "z": self.input_z},
+                fetch_list=[result_max],
+            )
+        np.testing.assert_allclose(res, self.np_expected2, rtol=1e-05)
+
+        with paddle.static.program_guard(
+            paddle.static.Program(), paddle.static.Program()
+        ):
+            data_a = paddle.static.data("a", shape=[3], dtype="int64")
+            data_c = paddle.static.data("c", shape=[3], dtype="int64")
+            result_max = paddle.subtract(data_a, data_c)
+            exe = paddle.static.Executor(self.place)
+            (res,) = exe.run(
+                feed={"a": self.input_a, "c": self.input_c},
+                fetch_list=[result_max],
+            )
+        np.testing.assert_allclose(res, self.np_expected3, rtol=1e-05)
+
+        with paddle.static.program_guard(
+            paddle.static.Program(), paddle.static.Program()
+        ):
+            data_b = paddle.static.data("b", shape=[3], dtype="int64")
+            data_c = paddle.static.data("c", shape=[3], dtype="int64")
+            result_max = paddle.subtract(data_b, data_c)
+            exe = paddle.static.Executor(self.place)
+            (res,) = exe.run(
+                feed={"b": self.input_b, "c": self.input_c},
+                fetch_list=[result_max],
+            )
+        np.testing.assert_allclose(res, self.np_expected4, rtol=1e-05)
+
+    def test_dynamic_api(self):
+        paddle.disable_static()
+        x = paddle.to_tensor(self.input_x)
+        y = paddle.to_tensor(self.input_y)
+        z = paddle.to_tensor(self.input_z)
+
+        a = paddle.to_tensor(self.input_a)
+        b = paddle.to_tensor(self.input_b)
+        c = paddle.to_tensor(self.input_c)
+
+        res = paddle.subtract(x, y)
+        res = res.numpy()
+        np.testing.assert_allclose(res, self.np_expected1, rtol=1e-05)
+
+        # test broadcast
+        res = paddle.subtract(x, z)
+        res = res.numpy()
+        np.testing.assert_allclose(res, self.np_expected2, rtol=1e-05)
+
+        res = paddle.subtract(a, c)
+        res = res.numpy()
+        np.testing.assert_allclose(res, self.np_expected3, rtol=1e-05)
+
+        res = paddle.subtract(b, c)
+        res = res.numpy()
+        np.testing.assert_allclose(res, self.np_expected4, rtol=1e-05)
