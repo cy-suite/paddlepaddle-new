651a9e68f8e9a7cab8ad2e6403c534fd3296552d
Junjie Zhang <1356732652@qq.com>
2025-02-27 12:25:07 +0800

    fix scale

--- test/ir/inference/test_trt_convert_scale.py
+++ test/ir/inference/test_trt_convert_scale.py
@@ -48,7 +48,7 @@ class TrtConvertScaleTest(TrtLayerAutoScanTest):
                     np.int32 if is_int else np.float32
                 )
             elif self.dims == 1:
-                return np.ones([24]).astype(np.int32 if is_int else np.float32)
+                return np.ones([4]).astype(np.int32 if is_int else np.float32)
             elif self.dims == 0:
                 return np.ones([]).astype(np.int32 if is_int else np.float32)
 
@@ -132,38 +132,40 @@ class TrtConvertScaleTest(TrtLayerAutoScanTest):
 
             yield program_config
 
+    def generate_dynamic_shape(self):
+        if self.dims == 4:
+            self.dynamic_shape.min_input_shape = {
+                "scale_input": [1, 3, 24, 24]
+            }
+            self.dynamic_shape.max_input_shape = {
+                "scale_input": [4, 3, 24, 24]
+            }
+            self.dynamic_shape.opt_input_shape = {
+                "scale_input": [1, 3, 24, 24]
+            }
+        elif self.dims == 3:
+            self.dynamic_shape.min_input_shape = {"scale_input": [1, 3, 24]}
+            self.dynamic_shape.max_input_shape = {"scale_input": [4, 3, 24]}
+            self.dynamic_shape.opt_input_shape = {"scale_input": [1, 3, 24]}
+        elif self.dims == 2:
+            self.dynamic_shape.min_input_shape = {"scale_input": [1, 24]}
+            self.dynamic_shape.max_input_shape = {"scale_input": [9, 48]}
+            self.dynamic_shape.opt_input_shape = {"scale_input": [1, 24]}
+        elif self.dims == 1:
+            self.dynamic_shape.min_input_shape = {"scale_input": [1]}
+            self.dynamic_shape.max_input_shape = {"scale_input": [8]}
+            self.dynamic_shape.opt_input_shape = {"scale_input": [4]}
+        elif self.dims == 0:
+            self.dynamic_shape.min_input_shape = {"scale_input": []}
+            self.dynamic_shape.max_input_shape = {"scale_input": []}
+            self.dynamic_shape.opt_input_shape = {"scale_input": []}
+        return self.dynamic_shape
+
     def sample_predictor_configs(
-        self, program_config
+        self, program_config, run_pir=False
     ) -> Generator[
         Any, Any, tuple[paddle_infer.Config, list[int], float] | None
     ]:
-        def generate_dynamic_shape(attrs):
-            if self.dims == 4:
-                self.dynamic_shape.min_input_shape = {
-                    "scale_input": [1, 3, 24, 24]
-                }
-                self.dynamic_shape.max_input_shape = {
-                    "scale_input": [4, 3, 24, 24]
-                }
-                self.dynamic_shape.opt_input_shape = {
-                    "scale_input": [1, 3, 24, 24]
-                }
-            elif self.dims == 3:
-                self.dynamic_shape.min_input_shape = {"scale_input": [1, 3, 24]}
-                self.dynamic_shape.max_input_shape = {"scale_input": [4, 3, 24]}
-                self.dynamic_shape.opt_input_shape = {"scale_input": [1, 3, 24]}
-            elif self.dims == 2:
-                self.dynamic_shape.min_input_shape = {"scale_input": [1, 24]}
-                self.dynamic_shape.max_input_shape = {"scale_input": [9, 48]}
-                self.dynamic_shape.opt_input_shape = {"scale_input": [1, 24]}
-            elif self.dims == 1:
-                self.dynamic_shape.min_input_shape = {"scale_input": [24]}
-                self.dynamic_shape.max_input_shape = {"scale_input": [48]}
-                self.dynamic_shape.opt_input_shape = {"scale_input": [24]}
-            elif self.dims == 0:
-                self.dynamic_shape.min_input_shape = {"scale_input": []}
-                self.dynamic_shape.max_input_shape = {"scale_input": []}
-                self.dynamic_shape.opt_input_shape = {"scale_input": []}
 
         def clear_dynamic_shape():
             self.dynamic_shape.min_input_shape = {}
@@ -181,19 +183,20 @@ class TrtConvertScaleTest(TrtLayerAutoScanTest):
 
         # for static_shape
         clear_dynamic_shape()
-        self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        program_config.set_input_type(np.float32)
-        yield self.create_inference_config(), generate_trt_nodes_num(
-            attrs, False
-        ), 1e-5
-        self.trt_param.precision = paddle_infer.PrecisionType.Half
-        program_config.set_input_type(np.float16)
-        yield self.create_inference_config(), generate_trt_nodes_num(
-            attrs, False
-        ), (1e-3, 1e-3)
+        if not run_pir:
+            self.trt_param.precision = paddle_infer.PrecisionType.Float32
+            program_config.set_input_type(np.float32)
+            yield self.create_inference_config(), generate_trt_nodes_num(
+                attrs, False
+            ), 1e-5
+            self.trt_param.precision = paddle_infer.PrecisionType.Half
+            program_config.set_input_type(np.float16)
+            yield self.create_inference_config(), generate_trt_nodes_num(
+                attrs, False
+            ), (1e-3, 1e-3)
 
         # for dynamic_shape
-        generate_dynamic_shape(attrs)
+        self.generate_dynamic_shape()
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
         program_config.set_input_type(np.float32)
         yield self.create_inference_config(), generate_trt_nodes_num(
@@ -230,7 +233,7 @@ class TrtConvertScaleTest(TrtLayerAutoScanTest):
 
     def test(self):
         self.add_skip_trt_case()
-        self.run_test()
+        self.run_test(run_pir=True)
 
 
 if __name__ == "__main__":
