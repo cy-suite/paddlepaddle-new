070e7da4b818e89f4cf16e47a3cac30f5829282e
liuruyan <1823192871@qq.com>
2025-02-27 06:27:59 +0000

    apply constraint in CINN backend

---
+++ paddle/cinn/common/shape_constraint.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2025 CINN Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+#include <unordered_set>
+#include <vector>
+#include "paddle/cinn/ir/ir.h"
+#include "paddle/cinn/ir/ir_base.h"
+#include "paddle/common/union_find_set.h"
+#include "paddle/pir/include/dialect/shape/utils/dim_expr.h"
+namespace cinn {
+namespace common {
+
+class ShapeConstraintManager {
+ public:
+  static ShapeConstraintManager& Instance();
+  void Init(const ::common::UnionFindSet<symbol::DimExpr>& equal_dim_exprs);
+  void Init(const ::common::UnionFindSet<ir::IndexExpr>& equal_exprs);
+  bool IsEqual(const ir::IndexExpr& lhs, const ir::IndexExpr& rhs);
+  const ::common::UnionFindSet<symbol::DimExpr>& GetDimExprEqual() const;
+  const ::common::UnionFindSet<ir::IndexExpr>& GetEqualExprs() const;
+
+ private:
+  ::common::UnionFindSet<symbol::DimExpr> equal_dim_exprs_;
+  ::common::UnionFindSet<ir::IndexExpr> equal_exprs_;
+  ShapeConstraintManager() = default;  // 私有构造函数
+  ~ShapeConstraintManager() = default;
+  ShapeConstraintManager(const ShapeConstraintManager&) = delete;
+  ShapeConstraintManager(ShapeConstraintManager&&) = delete;
+  ShapeConstraintManager& operator=(const ShapeConstraintManager&) = delete;
+};
+
+std::ostream& operator<<(std::ostream& os,
+                         const ShapeConstraintManager& constraints_manager);
+}  // namespace common
+}  // namespace cinn
--- paddle/cinn/hlir/framework/pir/op_lowering_impl.cc
+++ paddle/cinn/hlir/framework/pir/op_lowering_impl.cc
@@ -20,6 +20,7 @@
 #include "paddle/cinn/ast_gen_ius/tensor_group.h"
 #include "paddle/cinn/backends/codegen_device_util.h"
 #include "paddle/cinn/common/dim_expr_converter.h"
+#include "paddle/cinn/common/shape_constraint.h"
 #include "paddle/cinn/common/target.h"
 #include "paddle/cinn/hlir/dialect/operator/ir/manual_op.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/group_merge/op_with_group_merge_util.h"
@@ -91,6 +92,8 @@ OpLowererImpl::OpLowererImpl(const Target& target) : target_(target) {
 
 BucketLoweredFuncsWrapper OpLowererImpl::BucketLower(
     const OpLoweringGroupPtr& group) {
+  auto& constraint = cinn::common::ShapeConstraintManager::Instance();
+  std::cout << constraint << std::endl;
   VLOG(4) << "BucketLower Group : \n" << *group;
   // 1.Do compute, lower and schedule for each op.
   const auto& ops = group->ops();
