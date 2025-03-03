aa4e2fa99773f6ec553f80f37b1edc842248463b
ooooo <3164076421@qq.com>
2025-03-03 10:03:46 +0000

    fix bugs

--- paddle/pir/src/dialect/shape/utils/dim_expr_util.cc
+++ paddle/pir/src/dialect/shape/utils/dim_expr_util.cc
@@ -1007,29 +1007,36 @@ struct FoldRepetitiveSymbol {
 };
 
 bool IsLhsGreatThanRhs(const DimExpr& lhs, const DimExpr& rhs) {
-  auto LhsOperandsVisitor = common::Overloaded{
-      [&](const Mul<DimExpr>& mul) {
-        bool lhs_great_than_rhs = false;
-        for (const auto& expr : *mul.operands) {
-          if (expr == rhs)
-            lhs_great_than_rhs = true;
-          else if (!expr.isa<std::int64_t>() && !expr.isa<std::string>())
-            return false;
-        }
-        return lhs_great_than_rhs;
-      },
-      [&](const Add<DimExpr>& add) {
-        bool lhs_great_than_rhs = false;
-        for (const auto& expr : *add.operands) {
-          if (expr == rhs)
-            lhs_great_than_rhs = true;
-          else if (!expr.isa<std::int64_t>() && !expr.isa<std::string>())
-            return false;
-        }
-        return lhs_great_than_rhs;
-      },
-      [&](const auto& lhs) { return false; }};
-  return std::visit(LhsOperandsVisitor, lhs.variant());
+  auto LhsOperandsVisitor =
+      common::Overloaded{[&](const Mul<DimExpr>& mul) {
+                           bool lhs_great_than_rhs = false;
+                           for (const auto& expr : *mul.operands) {
+                             if (expr == rhs) {
+                               lhs_great_than_rhs = true;
+                             } else if (!(expr.isa<std::int64_t>() &&
+                                          expr.Get<std::int64_t>() > 0) &&
+                                        !expr.isa<std::string>()) {
+                               return false;
+                             }
+                           }
+                           return lhs_great_than_rhs;
+                         },
+                         [&](const Add<DimExpr>& add) {
+                           bool lhs_great_than_rhs = false;
+                           for (const auto& expr : *add.operands) {
+                             if (expr == rhs) {
+                               lhs_great_than_rhs = true;
+                             } else if (!(expr.isa<std::int64_t>() &&
+                                          expr.Get<std::int64_t>() > 0) &&
+                                        !expr.isa<std::string>()) {
+                               return false;
+                             }
+                           }
+                           return lhs_great_than_rhs;
+                         },
+                         [&](const auto& lhs) { return false; }};
+  return std::visit(LhsOperandsVisitor, lhs.variant()) ||
+         (symbol::Compare(lhs, rhs) == symbol::DimExprCompareResult::GT);
 }
 
 template <template <typename> class Op>
@@ -1494,22 +1501,31 @@ IR_API PriorityComparisonStatus CompareDimExprPriority(const DimExpr& lhs,
 }
 
 DimExprCompareResult Compare(const DimExpr& lhs, const DimExpr& rhs) {
+  auto CompareExpressions = [](const auto& lhs_val, const auto& rhs_val) {
+    DimExpr simplified_result =
+        SimplifyDimExpr(DimExpr{lhs_val} - DimExpr{rhs_val});
+    if (simplified_result.isa<int64_t>()) {
+      int64_t const_result = simplified_result.dyn_cast<int64_t>();
+      return const_result == 0  ? DimExprCompareResult::EQ
+             : const_result < 0 ? DimExprCompareResult::LT
+                                : DimExprCompareResult::GT;
+    }
+    return DimExprCompareResult::UNKNOWN;
+  };
   auto CompareFunc = common::Overloaded{
       [](const int& lhs, const int& rhs) {
         return lhs == rhs ? DimExprCompareResult::EQ
                           : (lhs < rhs ? DimExprCompareResult::LT
                                        : DimExprCompareResult::GT);
       },
-      [](const Add<DimExpr>& lhs, const Add<DimExpr>& rhs) {
-        DimExpr simplified_result =
-            SimplifyDimExpr(DimExpr{lhs} - DimExpr{rhs});
-        if (simplified_result.isa<int64_t>()) {
-          int64_t const_result = simplified_result.dyn_cast<int64_t>();
-          return const_result == 0  ? DimExprCompareResult::EQ
-                 : const_result < 0 ? DimExprCompareResult::LT
-                                    : DimExprCompareResult::GT;
-        }
-        return DimExprCompareResult::UNKNOWN;
+      [&CompareExpressions](const auto& lhs, const Add<DimExpr>& rhs) {
+        return CompareExpressions(lhs, rhs);
+      },
+      [&CompareExpressions](const Add<DimExpr>& lhs, const auto& rhs) {
+        return CompareExpressions(lhs, rhs);
+      },
+      [&CompareExpressions](const Add<DimExpr>& lhs, const Add<DimExpr>& rhs) {
+        return CompareExpressions(lhs, rhs);
       },
       [](const auto& lhs, const auto& rhs) {
         return DimExprCompareResult::UNKNOWN;
