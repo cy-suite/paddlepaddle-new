dc8d7f2129500f09cb96aa0477d103baff7d3b68
liuruyan <1823192871@qq.com>
2025-03-04 13:41:19 +0000

    fix ci

--- test/cpp/pir/cinn/adt/merge_block_utils_test.cc
+++ test/cpp/pir/cinn/adt/merge_block_utils_test.cc
@@ -16,6 +16,7 @@
 #include <gtest/gtest.h>
 #include <sstream>
 
+#include "paddle/cinn/ir/op/ir_operators.h"
 #include "paddle/cinn/optim/ir_simplify.h"
 #include "paddle/cinn/optim/merge_block_utils.h"
 
