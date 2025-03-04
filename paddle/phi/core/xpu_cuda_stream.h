9fd9c9bf296e1c9b8498b141bc5afb3b09ff6d16
tizhou86 <tizhou1986@gmail.com>
2025-03-04 10:52:50 +0800

    [XPU] feat: support xpu ipc and stream to enable zero cost checkpoint

--- paddle/phi/core/xpu_cuda_stream.h
+++ paddle/phi/core/xpu_cuda_stream.h
@@ -12,6 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#ifdef PADDLE_WITH_XPU
+
 #pragma once
 
 #include <cuda.h>
@@ -123,3 +125,5 @@ class XPUCUDAStream {
 };
 
 }  // namespace phi
+
+#endif
