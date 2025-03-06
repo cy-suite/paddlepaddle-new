9be52c025a8eba392eddc0601e2c11702aef4af1
DrRyanHuang <zihaohuang@aliyun.com>
2025-03-06 09:13:34 +0000

    refine error

--- python/paddle/jit/sot/opcode_translator/executor/variables/callable.py
+++ python/paddle/jit/sot/opcode_translator/executor/variables/callable.py
@@ -249,7 +249,7 @@ class UserDefinedFunctionVariable(FunctionVariable):
 
             raise BreakGraphError(
                 exception_class(
-                    f"{location_info} encountered breakgraph error caused by\n    {error}"
+                    f"{location_info} encountered breakgraph error caused by `{error}`"
                 )
             )
         return output
