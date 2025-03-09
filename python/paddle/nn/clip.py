4cbb01d48694a410254f5efffff2ebe281b5249e
Waynezee <wangxiangzhe@baidu.com>
2025-03-09 16:53:29 +0800

    use inplace multiply in grad_clip

--- python/paddle/nn/clip.py
+++ python/paddle/nn/clip.py
@@ -824,8 +824,12 @@ class ClipGradByGlobalNorm(ClipGradBase):
                         clip_input = paddle.distributed.reshard(
                             clip_input, g.process_mesh, clip_input.placements
                         )
-                g.multiply_(clip_input)
-                params_and_grads.append((p, g))
+                if g.is_dist() or g.is_dense():
+                    g.multiply_(clip_input)
+                    params_and_grads.append((p, g))
+                else:
+                    new_grad = paddle.multiply(g, clip_input)
+                    params_and_grads.append((p, new_grad))
             else:
                 params_and_grads.append((p, g))
 
@@ -1089,8 +1093,8 @@ class ClipGradByGlobalNorm(ClipGradBase):
                         ),
                     )
 
-                g.multiply_(clip_input)
-                params_and_grads.append((p, g))
+                new_grad = paddle.multiply(g, clip_input)
+                params_and_grads.append((p, new_grad))
             else:
                 params_and_grads.append((p, g))
 
