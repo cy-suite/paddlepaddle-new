ded13d430eb064ffbdc41cf3c7a90e4fbce8374b
gushiwei <gushiwei@baidu.com>
2025-03-14 09:50:26 +0000

    test=document_fix

--- ci/coverage_check_added_ut.sh
+++ ci/coverage_check_added_ut.sh
@@ -34,7 +34,7 @@ git branch
 mkdir prec_build
 cd prec_build
 
-source $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/utils.sh
+source ${PADDLE_ROOT}/ci/utils.sh
 init
 cmake_base ${PYTHON_ABI:-""} >prebuild.log 2>&1
 
