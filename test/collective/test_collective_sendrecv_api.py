aaa9b92fdc04b7b309f1e67a434c7cc053d56179
mikethegoblin <mikegu0508@gmail.com>
2025-03-17 17:21:12 +0800

    remove unit tests

--- test/collective/test_collective_sendrecv_api.py
+++ test/collective/test_collective_sendrecv_api.py
@@ -53,26 +53,6 @@ class TestCollectiveSendRecvAPI(TestDistBase):
                     need_envs={"USE_COMM_CONTEXT": "1"},
                 )
 
-    def test_sendrecv_flagcx_with_comm_context(self):
-        dtypes_to_test = [
-            "float16",
-            "float32",
-            "float64",
-            "int32",
-            "int64",
-            "int8",
-            "uint8",
-            "bool",
-        ]
-        for dtype in dtypes_to_test:
-            self.check_with_place(
-                "collective_sendrecv_api.py",
-                "sendrecv",
-                "flagcx",
-                dtype=dtype,
-                need_envs={"USE_COMM_CONTEXT": "1"},
-            )
-
     def test_sendrecv_nccl_dygraph(self):
         dtypes_to_test = [
             "float16",
@@ -91,26 +71,6 @@ class TestCollectiveSendRecvAPI(TestDistBase):
                 "collective_sendrecv_api_dygraph.py",
                 "sendrecv",
                 "nccl",
-                static_mode="0",
-                dtype=dtype,
-            )
-
-    def test_sendrecv_flagcx_dygraph(self):
-        dtypes_to_test = [
-            "float16",
-            "float32",
-            "float64",
-            "int32",
-            "int64",
-            "int8",
-            "uint8",
-            "bool",
-        ]
-        for dtype in dtypes_to_test:
-            self.check_with_place(
-                "collective_sendrecv_api_dygraph.py",
-                "sendrecv",
-                "flagcx",
                 static_mode="0",
                 dtype=dtype,
             )
