c254fa898cd2fbf703e1677b2e5a90fb9b7d2a9d
0x3878f <1757941716@qq.com>
2025-03-17 07:02:57 +0000

    Set stop_gradient=False and Skip the test when not is_compiled_with_distribute

--- test/sot/test_sot_distribution.py
+++ test/sot/test_sot_distribution.py
@@ -29,15 +29,25 @@ def fn(x, y):
     return x + y
 
 
+@unittest.skipIf(
+    not paddle.is_compiled_with_distribute(),
+    reason='Not compiled with distribute.',
+)
 class TestGuardForDistInfo(TestCaseBase):
     def test_fn(self):
         x = paddle.ones([2, 2])
+        x.stop_gradient = False
         y = paddle.zeros([2, 2])
+        y.stop_gradient = False
         mesh1 = dist.ProcessMesh([0, 1], dim_names=['x'])
         mesh2 = dist.ProcessMesh([0, 1], dim_names=['y'])
         mesh3 = dist.ProcessMesh([0, 2], dim_names=['x'])
-        dist_x1 = dist.shard_tensor(x, mesh1, [dist.Replicate()])
-        dist_y1 = dist.shard_tensor(y, mesh1, [dist.Replicate()])
+        dist_x1 = dist.shard_tensor(
+            x, mesh1, [dist.Replicate()], stop_gradient=False
+        )
+        dist_y1 = dist.shard_tensor(
+            y, mesh1, [dist.Replicate()], stop_gradient=False
+        )
         dist_x2 = dist.shard_tensor(x, mesh2, [dist.Replicate()])
         dist_y2 = dist.shard_tensor(y, mesh2, [dist.Replicate()])
         dist_x3 = dist.shard_tensor(x, mesh3, [dist.Replicate()])
