d40c3d139a1d6361cd739c4938279d80dc816dfe
Waynezee <wangxiangzhe@baidu.com>
2025-03-17 09:04:20 +0800

    fix loss scale in xpu

--- python/paddle/optimizer/optimizer.py
+++ python/paddle/optimizer/optimizer.py
@@ -1336,7 +1336,11 @@ class Optimizer:
 
             if framework.in_dygraph_mode():
                 found_inf = self._get_auxiliary_var('found_inf')
-                if "xpu" in paddle.device.get_device() and found_inf.is_dist():
+                if (
+                    "xpu" in paddle.device.get_device()
+                    and found_inf is not None
+                    and found_inf.is_dist()
+                ):
                     found_inf = found_inf._local_value()
                 if found_inf:
                     if isinstance(found_inf, core.eager.Tensor):
