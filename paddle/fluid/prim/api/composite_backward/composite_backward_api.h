ee063716784e76eca3566995c54a136cdfe23beb
Wang Huan <wanghuan29@baidu.com>
2025-03-18 06:22:51 +0000

    refine

--- paddle/fluid/prim/api/composite_backward/composite_backward_api.h
+++ paddle/fluid/prim/api/composite_backward/composite_backward_api.h
@@ -1416,7 +1416,7 @@ void maximum_grad(const Tensor& x,
   Tensor half_tensor;
   if (x_grad || y_grad) {
     auto equal_tensor = cast<T>(equal<T>(x, y), out_grad.dtype());
-    auto two_tensor = full_scalar<T>(2.0, out_grad.dtype());
+    auto two_tensor = full<T>({1}, 2.0, out_grad.dtype(), out_grad.place());
     half_tensor = (out_grad / two_tensor) * equal_tensor;
   }
 
@@ -1907,7 +1907,7 @@ void minimum_grad(const Tensor& x,
   Tensor half_tensor;
   if (x_grad || y_grad) {
     auto equal_tensor = cast<T>(equal<T>(x, y), out_grad.dtype());
-    auto two_tensor = full_scalar<T>(2.0, out_grad.dtype());
+    auto two_tensor = full<T>({1}, 2.0, out_grad.dtype(), out_grad.place());
     half_tensor = (out_grad / two_tensor) * equal_tensor;
   }
 
