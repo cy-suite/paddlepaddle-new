28765b4702d92d534dd143db02ff4a7bc5a593e0
risemeup1 <62429225+risemeup1@users.noreply.github.com>
2025-03-19 17:35:35 +0800

    Update gather_grad_kernel.cu

--- paddle/phi/kernels/gpu/gather_grad_kernel.cu
+++ paddle/phi/kernels/gpu/gather_grad_kernel.cu
@@ -76,6 +76,5 @@ PD_REGISTER_KERNEL(gather_grad,
                    int,
                    phi::dtype::float16,
                    phi::dtype::bfloat16,
-                   phi::dtype::float8_e4m3fn,
                    phi::dtype::complex<float>,
                    phi::dtype::complex<double>) {}
