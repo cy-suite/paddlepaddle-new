b17fbc5c121eb2d1cd7c52012c9e2f4cee18dfa4
ooooo <3164076421@qq.com>
2025-03-26 09:47:22 +0000

    fix

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
@@ -44,11 +44,11 @@ inline void UpdatePaddingAndDilation(
     symbol::DimExprBuilder builder;
     for (size_t i = 0; i < data_dims.size(); ++i) {
       symbol::DimExpr out_size = (data_dims[i] + strides[i] - 1) / strides[i];
+      symbol::DimExpr pad_sum;
       if (strides[i] == 2 && ksize[i] == symbol::DimExpr{3}) {
-        symbol::DimExpr pad_sum =
-            (out_size - one) * strides[i] + ksize[i] - data_dims[i];
+        pad_sum = (out_size - one) * strides[i] + ksize[i] - data_dims[i];
       } else {
-        symbol::DimExpr pad_sum = builder.Max(
+        pad_sum = builder.Max(
             (out_size - one) * strides[i] + ksize[i] - data_dims[i], zero);
       }
 
