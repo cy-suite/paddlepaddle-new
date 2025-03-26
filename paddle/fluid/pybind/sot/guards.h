a6b39835a082a2db87d7b442200ea32f3627b87b
gouzi <530971494@qq.com>
2025-03-26 23:14:56 +0800

    fix build error

--- paddle/fluid/pybind/sot/guards.h
+++ paddle/fluid/pybind/sot/guards.h
@@ -25,6 +25,14 @@ namespace py = pybind11;
 #define PYBIND11_DETAILED_ERROR_MESSAGES
 #if SOT_IS_SUPPORTED
 
+// The visibility attribute is to avoid a warning about storing a field in the
+// struct that has a different visibility (from pybind) than the struct.
+#ifdef _WIN32
+#define VISIBILITY_HIDDEN
+#else
+#define VISIBILITY_HIDDEN __attribute__((visibility("hidden")))
+#endif
+
 class GuardBase {
  public:
   GuardBase() = default;
