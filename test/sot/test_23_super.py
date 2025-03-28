a152716586b1a5a5adb5a6b314a5bfb4173b028e
DrRyanHuang <zihaohuang@aliyun.com>
2025-03-28 02:53:19 +0000

    adapt assert_exceptions 4 py3.13

--- test/sot/test_23_super.py
+++ test/sot/test_23_super.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
 import types
 import unittest
 
@@ -345,13 +346,22 @@ class TestCustomSuper(TestCaseBase):
         )
 
     def test_super_function_as_input(self):
-        self.assert_exceptions(
-            InnerError,
-            "KeyError: '__class__'",
-            FakeSuperClass().super_function_as_input,
-            super,
-            paddle.to_tensor(3.0),
-        )
+        if sys.version_info >= (3, 13):
+            self.assert_exceptions(
+                RuntimeError,
+                r"super\(\): __class__ cell not found",
+                FakeSuperClass().super_function_as_input,
+                super,
+                paddle.to_tensor(3.0),
+            )
+        if sys.version_info < (3, 13):
+            self.assert_exceptions(
+                InnerError,
+                "KeyError: '__class__'",
+                FakeSuperClass().super_function_as_input,
+                super,
+                paddle.to_tensor(3.0),
+            )
 
 
 if __name__ == "__main__":
