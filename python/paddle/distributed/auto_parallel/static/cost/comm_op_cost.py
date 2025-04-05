f8dd4d9b75a64260f58916ec0a89327dc3c1915e
co63oc <co63oc@users.noreply.github.com>
2025-04-05 20:06:06 +0800

    Fix

--- python/paddle/distributed/auto_parallel/static/cost/comm_op_cost.py
+++ python/paddle/distributed/auto_parallel/static/cost/comm_op_cost.py
@@ -23,6 +23,68 @@ from .base_cost import CommOpCost, register_op_cost
 
 @register_op_cost
 class AllreduceSumOpCost(CommOpCost):
+    OP_TYPE = "c_allreduce_sum"
+
+    def __init__(self, op=None, op_desc=None, comm_context=None):
+        super().__init__(op=op, op_desc=op_desc, comm_context=comm_context)
+
+    def calc_time(self):
+        # use tree if cross machine and use ring if in a single machine
+        time = None
+        cluster = self.comm_context.cluster
+        if not cluster.cross_machine(self.group_ranks):
+            time = self.calc_time_ring()
+        else:
+            time = self.calc_time_tree()
+
+        return time
+
+    def calc_time_ring(self):
+        alpha = self.comm_context.base_ring
+        alpha += (
+            2
+            * (self.rank_count - self.machine_count)
+            * self.comm_context.intra_ring
+        )
+        alpha += (
+            2
+            * (self.machine_count - 1)
+            * (
+                self.comm_context.inter_ring
+                + self.hops * self.comm_context.switch
+            )
+        )
+        beta = self.comm_context.get_max_beta(self.group_ranks)
+        time = (
+            alpha
+            + 2
+            * (self.rank_count - 1)
+            / self.rank_count
+            * self.comm_count
+            * beta
+        )
+
+        return time
+
+    def calc_time_tree(self):
+        alpha = self.comm_context.base_tree
+        alpha += (
+            2
+            * (self.rank_count / self.machine_count - 1)
+            * self.comm_context.intra_tree
+        )
+        alpha += math.log2(self.machine_count) * (
+            self.comm_context.inter_tree + self.hops * self.comm_context.switch
+        )
+        beta = self.comm_context.get_max_beta(self.group_ranks)
+
+        time = alpha + 2 * self.comm_count * beta
+
+        return time
+
+
+@register_op_cost
+class AllReduceOpCost(CommOpCost):
     OP_TYPE = "all_reduce"
 
     def __init__(self, op=None, op_desc=None, comm_context=None):
